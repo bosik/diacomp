9af82e00d3dce834a2636b46cb9c21234fe60669
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-10-29 19:34:26 +0000

    Error handler updated.

--- trunk/android/Comp/src/org/bosik/compensation/utils/ErrorHandler.java
+++ trunk/android/Comp/src/org/bosik/compensation/utils/ErrorHandler.java
@@ -19,39 +19,75 @@ public class ErrorHandler
 		// ErrorHandler.handler = new ErrorHandler();
 	}
 
+	private static String safe(String s)
+	{
+		return (s != null ? s : "null");
+	}
+
 	public static void handle(Throwable e, Activity activity)
 	{
 		if (BuildConfig.DEBUG)
 		{
-			Log.e(TAG, "Error handler [mode: debug]: " + e.getMessage());
-			throw new RuntimeException(e);
+			if (e != null)
+			{
+				Log.e(TAG, "Error handler [mode: debug]: " + e.getLocalizedMessage());
+				throw new RuntimeException(e);
+			} else
+			{
+				Log.e(TAG, "Error handler [mode: debug]: (e == null)");
+				throw new RuntimeException();
+			}
 		} else
 		{
-			Log.e(TAG, "Error handler [mode: release]: " + e.getMessage());
-
 			try
 			{
-				String trace = Log.getStackTraceString(e);
+				if (e != null)
+					Log.e(TAG, "Error handler [mode: release]: " + e.getLocalizedMessage());
+				else
+					Log.e(TAG, "Error handler [mode: release]: (e == null)");
+
+				if (activity != null)
+				{
+					UIUtils.showTip(activity, "Произошла ошибка");
+				}
+
+				String trace = (e != null ? Log.getStackTraceString(e) : "(e == null)");
+				String exc = (e != null ? e.getLocalizedMessage() : "(e == null)");
 
 				StringBuilder msg = new StringBuilder();
-				msg.append("<b>" + TAG + " has detected the exception during user runtime.</b>\n\n");
+				msg.append("<b>" + TAG + " has detected exception during user runtime</b>\n\n");
 				msg.append("<b>Date:</b> " + Utils.now().toGMTString() + "\n");
-				msg.append("<b>User:</b> " + webClient.getUsername() + "\n");
-				msg.append("<b>Exception:</b> <font name=\"Courier New\">" + e.getMessage() + "</font>\n\n");
+				msg.append("<b>User:</b> "
+						+ (webClient != null ? safe(webClient.getUsername()) : "(webClient == null)") + "\n");
+				msg.append("<b>Exception:</b>" + exc + "\n\n");
+				msg.append("<font name='Courier New'>");
 				msg.append(trace);
+				msg.append("</font>");
 
 				String m = msg.toString();
 				Log.e(TAG, m);
 
-				boolean sent = webClient.sendMail(m.replace("\n", "<br/>"));
-
-				if ((activity != null) && sent)
+				if (webClient != null)
 				{
-					UIUtils.showTip(activity,
-							"Произошла ошибка. Отчёт с технической информацией отправлен разработчику.");
+					boolean sent = webClient.sendMail(m.replace("\n", "<br/>"));
+
+					if (activity != null)
+					{
+						if (sent)
+							UIUtils.showTip(activity, "Отчёт с технической информацией отправлен разработчику");
+						else
+							UIUtils.showTip(activity, "Отправить отчёт не удалось");
+					}
+				} else
+				{
+					if (activity != null)
+					{
+						UIUtils.showTip(activity, "Отправить отчёт не удалось (webClient == null)");
+					}
 				}
 			} catch (Exception ex)
 			{
+				UIUtils.showTip(activity, "Во время обработки ошибки произошла ещё одна ошибка :(");
 				// well, something bad has occurred and we have no way to talk about it :(
 				// ok, let's print it for some kind of smart user
 				ex.printStackTrace();
