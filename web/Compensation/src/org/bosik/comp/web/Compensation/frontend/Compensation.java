a98768ede04cceb6b32d2c7e5e53b4cdc4aa8185
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-11-10 02:05:03 +0000

    Login via Google Account implemented; "Client" renamed to "frontend"; Localization service added.

--- web/Compensation/src/org/bosik/comp/web/Compensation/frontend/Compensation.java
+++ web/Compensation/src/org/bosik/comp/web/Compensation/frontend/Compensation.java
@@ -1,4 +1,4 @@
-package org.bosik.comp.web.Compensation.client;
+package org.bosik.comp.web.Compensation.frontend;
 
 import org.bosik.comp.web.Compensation.shared.FieldVerifier;
 import com.google.gwt.core.client.EntryPoint;
@@ -9,6 +9,7 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
@@ -22,27 +23,80 @@ import com.google.gwt.user.client.ui.VerticalPanel;
  */
 public class Compensation implements EntryPoint
 {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String			SERVER_ERROR	= "An error occurred while "
-																+ "attempting to contact the server. Please check your network "
-																+ "connection and try again.";
+	// String resources
+	// private static final String MESSAGE_ERROR_CONNECTION =
+	// "При подключении возникла ошибка. Повторите попытку.";
+	private static final String			MESSAGE_ERROR_JS_DISABLED	= "Для корректной работы приложения ваш браузер должен выполнять Java Script";
+	private static final String			LABEL_LOGIN_OFFER			= "Войдите с помощью учётной записи Google";
+	private static final String			LABEL_SIGN_IN				= "Вход";
+	private static final String			LABEL_SIGN_OUT				= "Выход";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync	greetingService	= GWT.create(GreetingService.class);
+	// Services
+	private final GreetingServiceAsync	greetingService				= GWT.create(GreetingService.class);
+	private final Localization			localization				= GWT.create(Localization.class);
+
+	// Components
+	private LoginInfo					loginInfo					= null;
+	private VerticalPanel				loginPanel					= new VerticalPanel();
+	private VerticalPanel				mainPanel					= new VerticalPanel();
+	private Label						loginLabel					= new Label(LABEL_LOGIN_OFFER);
+	private Anchor						signInLink					= new Anchor(LABEL_SIGN_IN);
+	private Anchor						signOutLink					= new Anchor(LABEL_SIGN_OUT);
 
 	/**
 	 * This is the entry point method.
 	 */
+	@Override
 	public void onModuleLoad()
 	{
+		// Check login status using login service.
+		LoginServiceAsync loginService = GWT.create(LoginService.class);
+		loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<LoginInfo>()
+		{
+			@Override
+			public void onFailure(Throwable error)
+			{
+			}
+
+			@Override
+			public void onSuccess(LoginInfo result)
+			{
+				loginInfo = result;
+				if (loginInfo.isLoggedIn())
+				{
+					loadCompensation();
+				}
+				else
+				{
+					loadLogin();
+				}
+			}
+		});
+	}
+
+	private void loadLogin()
+	{
+		// Assemble login panel.
+		signInLink.setHref(loginInfo.getLoginUrl());
+		loginPanel.add(loginLabel);
+		loginPanel.add(signInLink);
+		RootPanel.get("stockList").add(loginPanel);
+
+	}
+
+	private void loadCompensation()
+	{
+		RootPanel.get("labelErrorNoJS").add(new Label(MESSAGE_ERROR_JS_DISABLED));
+
+		// Set up sign out hyperlink.
+		signOutLink.setHref(loginInfo.getLogoutUrl());
+
+		System.out.println("Logout URL: " + loginInfo.getLogoutUrl());
+
 		final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
+		nameField.setText(loginInfo.getNickname());
+		final Label welcomeLabel = new Label();
 		final Label errorLabel = new Label();
 
 		// We can add style names to widgets
@@ -50,9 +104,13 @@ public class Compensation implements EntryPoint
 
 		// Add the nameField and sendButton to the RootPanel
 		// Use RootPanel.get() to get the entire body element
+		RootPanel.get("labelWelcome").add(welcomeLabel);
 		RootPanel.get("nameFieldContainer").add(nameField);
 		RootPanel.get("sendButtonContainer").add(sendButton);
 		RootPanel.get("errorLabelContainer").add(errorLabel);
+
+		welcomeLabel.setText("Hello!");
+		welcomeLabel.setTitle("Tooltip");
 
 		// Focus the cursor on the name field when the app loads
 		nameField.setFocus(true);
@@ -80,6 +138,7 @@ public class Compensation implements EntryPoint
 		// Add a handler to close the DialogBox
 		closeButton.addClickHandler(new ClickHandler()
 		{
+			@Override
 			public void onClick(ClickEvent event)
 			{
 				dialogBox.hide();
@@ -94,6 +153,7 @@ public class Compensation implements EntryPoint
 			/**
 			 * Fired when the user clicks on the sendButton.
 			 */
+			@Override
 			public void onClick(ClickEvent event)
 			{
 				sendNameToServer();
@@ -102,6 +162,7 @@ public class Compensation implements EntryPoint
 			/**
 			 * Fired when the user types in the nameField.
 			 */
+			@Override
 			public void onKeyUp(KeyUpEvent event)
 			{
 				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER)
@@ -130,16 +191,18 @@ public class Compensation implements EntryPoint
 				serverResponseLabel.setText("");
 				greetingService.greetServer(textToServer, new AsyncCallback<String>()
 				{
+					@Override
 					public void onFailure(Throwable caught)
 					{
 						// Show the RPC error message to the user
 						dialogBox.setText("Remote Procedure Call - Failure");
 						serverResponseLabel.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(SERVER_ERROR);
+						serverResponseLabel.setHTML(localization.messageErrorConnection());
 						dialogBox.center();
 						closeButton.setFocus(true);
 					}
 
+					@Override
 					public void onSuccess(String result)
 					{
 						dialogBox.setText("Remote Procedure Call");
@@ -156,5 +219,21 @@ public class Compensation implements EntryPoint
 		MyHandler handler = new MyHandler();
 		sendButton.addClickHandler(handler);
 		nameField.addKeyUpHandler(handler);
+
+		mainPanel.add(signOutLink);
+		RootPanel.get("stockList").add(mainPanel);
 	}
+
+	// public String getUserNickname()
+	// {
+	// User user = UserServiceFactory.getUserService().getCurrentUser();
+	// if (user == null)
+	// {
+	// return "null";
+	// }
+	// else
+	// {
+	// return user.getNickname();
+	// }
+	// }
 }
--- web/Compensation/src/org/bosik/comp/web/Compensation/frontend/GreetingService.java
+++ web/Compensation/src/org/bosik/comp/web/Compensation/frontend/GreetingService.java
@@ -1,4 +1,4 @@
-package org.bosik.comp.web.Compensation.client;
+package org.bosik.comp.web.Compensation.frontend;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
