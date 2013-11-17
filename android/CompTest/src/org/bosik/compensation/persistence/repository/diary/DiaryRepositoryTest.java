6a10d8de6db2fd47c224b557fd0b3dc686ed4bbe
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-11-17 20:18:48 +0000

    Local repository persistence test added.

---
+++ android/CompTest/src/org/bosik/compensation/persistence/repository/diary/DiaryRepositoryTest.java
@@ -0,0 +1,57 @@
+package org.bosik.compensation.persistence.repository.diary;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.bosik.compensation.bo.diary.DiaryPage;
+import android.test.AndroidTestCase;
+
+public abstract class DiaryRepositoryTest extends AndroidTestCase
+{
+	private DiaryRepository	repository;
+
+	protected abstract DiaryRepository getRepository();
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		repository = getRepository();
+	}
+
+	public void testPersistanceSingle()
+	{
+		DiaryPage org = DiaryPageUtils.demoPageA();
+		repository.postPage(org);
+		// ------------------
+		DiaryPage restored = repository.getPage(org.getDate());
+		DiaryPageUtils.comparePages(org, restored);
+	}
+
+	public void testPersistanceMultiple()
+	{
+		DiaryPage orgA = DiaryPageUtils.demoPageA();
+		DiaryPage orgB = DiaryPageUtils.demoPageB();
+		List<DiaryPage> pages = new ArrayList<DiaryPage>();
+		pages.add(orgA);
+		pages.add(orgB);
+		repository.postPages(pages);
+
+		List<Date> dates = new ArrayList<Date>();
+		for (DiaryPage page : pages)
+		{
+			dates.add(page.getDate());
+		}
+		// ------------------
+		List<DiaryPage> restored = repository.getPages(dates);
+		assertEquals(pages.size(), restored.size());
+		for (int i = 0; i < pages.size(); i++)
+		{
+			DiaryPageUtils.comparePages(pages.get(i), restored.get(i));
+		}
+	}
+
+	public void testGetModList()
+	{
+		// fail("Not yet implemented"); // TODO
+	}
+}
--- android/CompTest/src/org/bosik/compensation/persistence/repository/providers/WebClientTest.java
+++ android/CompTest/src/org/bosik/compensation/persistence/repository/providers/WebClientTest.java
@@ -1,76 +1,55 @@
 package org.bosik.compensation.persistence.repository.providers;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
 import junit.framework.TestCase;
-import android.util.Log;
 
 public class WebClientTest extends TestCase
 {
-	private static final String	TAG			= WebClientTest.class.getSimpleName();
-
-	private static final String	SERVER		= "http://diacomp.16mb.com/";
-	private static final String	USERNAME	= "bosik-007@narod.ru";
-	private static final String	PASSWORD	= "devel0pment";
-	private static final int	TIMEOUT		= 3000;
+	private static final String	TAG	= WebClientTest.class.getSimpleName();
 
 	private WebClient			webClient;
 
-	@Override
-	protected void setUp() throws Exception
+	public static WebClient getWebClient()
 	{
-		super.setUp();
+		final String SERVER = "http://diacomp.16mb.com/";
+		final String USERNAME = "bosik-007@narod.ru";
+		final String PASSWORD = "devel0pment";
+		final int TIMEOUT = 3000;
 
-		webClient = new WebClient(TIMEOUT);
-		webClient.setUsername(USERNAME);
-		webClient.setPassword(PASSWORD);
-		webClient.setServer(SERVER);
+		WebClient client = new WebClient(TIMEOUT);
+		client.setUsername(USERNAME);
+		client.setPassword(PASSWORD);
+		client.setServer(SERVER);
 
-		webClient.login();
+		return client;
 	}
 
-	@Override
-	protected void tearDown() throws Exception
-	{
-		super.tearDown();
-	}
+	// TODO: implements
 
-	public void testLocalToServer()
-	{
-		Date localTime = Calendar.getInstance().getTime();
-		Date serverTime = webClient.localToServer(localTime);
-		Date localTimeAgain = webClient.serverToLocal(serverTime);
+	// @Override
+	// protected void setUp() throws Exception
+	// {
+	// super.setUp();
+	//
+	// webClient = new WebClient(TIMEOUT);
+	// webClient.setUsername(USERNAME);
+	// webClient.setPassword(PASSWORD);
+	// webClient.setServer(SERVER);
+	//
+	// webClient.login();
+	// }
 
-		assertEquals(localTime.getTime(), localTimeAgain.getTime());
-	}
+	// public void testLocalToServer()
+	// {
+	// Date localTime = Calendar.getInstance().getTime();
+	// Date serverTime = webClient.localToServer(localTime);
+	// Date localTimeAgain = webClient.serverToLocal(serverTime);
+	//
+	// assertEquals(localTime.getTime(), localTimeAgain.getTime());
+	// }
 
-	public void testIsOnlineBoolean()
-	{
-		assertTrue(webClient.isOnline());
-		assertTrue(webClient.isOnline(true));
-	}
-
-	public void testGetPages()
-	{
-		List<Date> dates = new ArrayList<Date>();
-
-		GregorianCalendar gc = new GregorianCalendar(2013, 04 - 1, 02);
-
-		// dates.add(new Date(2013, 04 - 1, 02));
-		dates.add(gc.getTime());
-
-		String resp = webClient.getPages(dates);
-
-		Log.v(TAG, "Page is " + resp);
-	}
-
-	public void testGetFoodBase()
-	{
-		String resp = webClient.getFoodBase();
-
-		Log.v(TAG, "FoodBase is " + resp);
-	}
+	// public void testIsOnlineBoolean()
+	// {
+	// assertTrue(webClient.isOnline());
+	// assertTrue(webClient.isOnline(true));
+	// }
 }
