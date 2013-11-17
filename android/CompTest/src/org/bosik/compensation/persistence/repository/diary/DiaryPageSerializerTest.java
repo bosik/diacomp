600329c0ef47646ba8ccd4c7de46e915b55a25f7
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-11-17 01:05:57 +0000

    Persistence methods moved from DiaryPage to DiaryPageSerializer.

--- android/CompTest/src/org/bosik/compensation/persistence/repository/diary/DiaryPageSerializerTest.java
+++ android/CompTest/src/org/bosik/compensation/persistence/repository/diary/DiaryPageSerializerTest.java
@@ -1,104 +1,19 @@
 package org.bosik.compensation.persistence.repository.diary;
 
-import java.util.Date;
 import junit.framework.TestCase;
-import org.bosik.compensation.persistence.entity.common.FoodMassed;
 import org.bosik.compensation.persistence.entity.diary.DiaryPage;
-import org.bosik.compensation.persistence.entity.diary.records.BloodRecord;
-import org.bosik.compensation.persistence.entity.diary.records.InsRecord;
-import org.bosik.compensation.persistence.entity.diary.records.MealRecord;
-import org.bosik.compensation.persistence.entity.diary.records.NoteRecord;
 import org.bosik.compensation.persistence.repository.common.Serializer;
 
 public class DiaryPageSerializerTest extends TestCase
 {
 	private final Serializer<DiaryPage>	serializer	= new DiaryPageSerializer();
 
-	private static DiaryPage demoPageA()
-	{
-		DiaryPage page = new DiaryPage();
-		page.setDate(new Date(2002, 03, 15));
-		page.add(new BloodRecord(127, 4.9, 3));
-		page.add(new InsRecord(135, 12));
-
-		MealRecord meal = new MealRecord(201, true);
-		meal.add(new FoodMassed("Карбонат \"Восточный\" (Черн)", 9.9, 26.3, 0, 276, 90));
-		meal.add(new FoodMassed("Хлеб чёрный \"Премиум\"", 5.5, 0.9, 44.1, 206.3, 42));
-
-		page.add(meal);
-		page.add(new NoteRecord(230, "Демо"));
-
-		return page;
-	}
-
-	private static DiaryPage demoPageB()
-	{
-		DiaryPage page = new DiaryPage();
-		page.setDate(new Date(2002, 03, 16));
-		page.add(new BloodRecord(820, 6.8, 7));
-		page.add(new InsRecord(829, 16));
-		page.add(new MealRecord(850, false));
-		page.add(new NoteRecord(1439, "DTest Again"));
-
-		return page;
-	}
-
-	private static void comparePages(DiaryPage expPage, DiaryPage actPage)
-	{
-		// check the header
-		assertEquals(expPage.getDate().getTime(), actPage.getDate().getTime(), 5 * 1000);
-		assertEquals(expPage.getTimeStamp().getTime(), actPage.getTimeStamp().getTime(), 5 * 1000);
-		assertEquals(expPage.getVersion(), actPage.getVersion());
-
-		// check body
-		assertEquals(expPage.count(), actPage.count());
-		for (int i = 0; i < expPage.count(); i++)
-		{
-			assertEquals(expPage.get(i).getTime(), actPage.get(i).getTime());
-
-			if (expPage.get(i).getClass() == BloodRecord.class)
-			{
-				assertEquals(((BloodRecord) expPage.get(i)).getValue(), ((BloodRecord) actPage.get(i)).getValue());
-				assertEquals(((BloodRecord) expPage.get(i)).getFinger(), ((BloodRecord) actPage.get(i)).getFinger());
-			}
-			else
-				if (expPage.get(i).getClass() == InsRecord.class)
-				{
-					assertEquals(((InsRecord) expPage.get(i)).getValue(), ((InsRecord) actPage.get(i)).getValue());
-				}
-				else
-					if (expPage.get(i).getClass() == MealRecord.class)
-					{
-						MealRecord expMeal = (MealRecord) expPage.get(i);
-						MealRecord actMeal = (MealRecord) actPage.get(i);
-
-						assertEquals(expMeal.getShortMeal(), actMeal.getShortMeal());
-						assertEquals(expMeal.count(), actMeal.count());
-
-						for (int j = 0; j < expMeal.count(); j++)
-						{
-							assertEquals(expMeal.get(j).getName(), actMeal.get(j).getName());
-							assertEquals(expMeal.get(j).getRelProts(), actMeal.get(j).getRelProts());
-							assertEquals(expMeal.get(j).getRelFats(), actMeal.get(j).getRelFats());
-							assertEquals(expMeal.get(j).getRelCarbs(), actMeal.get(j).getRelCarbs());
-							assertEquals(expMeal.get(j).getRelValue(), actMeal.get(j).getRelValue());
-							assertEquals(expMeal.get(j).getMass(), actMeal.get(j).getMass());
-						}
-					}
-
-			if (expPage.get(i).getClass() == NoteRecord.class)
-			{
-				assertEquals(((NoteRecord) expPage.get(i)).getText(), ((NoteRecord) actPage.get(i)).getText());
-			}
-		}
-	}
-
 	public void testPersistence()
 	{
-		DiaryPage org = demoPageA();
-		String source = org.writeFull();
+		DiaryPage org = DiaryPageUtils.demoPageA();
+		String source = serializer.write(org);
 
-		DiaryPage restored = DiaryPage.multiRead(source).get(0);
-		comparePages(org, restored);
+		DiaryPage restored = serializer.read(source);
+		DiaryPageUtils.comparePages(org, restored);
 	}
 }
