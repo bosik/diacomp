f955ca39732ac3d02c99e24947b5fcfd25e993be
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-11-17 01:05:57 +0000

    Persistence methods moved from DiaryPage to DiaryPageSerializer.

---
+++ trunk/android/CompTest/src/org/bosik/compensation/persistence/repository/diary/LocalDiaryRepositoryTest.java
@@ -0,0 +1,57 @@
+package org.bosik.compensation.persistence.repository.diary;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.bosik.compensation.persistence.entity.diary.DiaryPage;
+import android.content.ContentResolver;
+import android.test.AndroidTestCase;
+
+public class LocalDiaryRepositoryTest extends AndroidTestCase
+{
+	private DiaryRepository	repository;
+
+	@Override
+	protected void setUp() throws Exception
+	{
+		ContentResolver resolver = getContext().getContentResolver();
+		repository = new LocalDiaryRepository(resolver);
+	}
+
+	public void testPersistanceSingle()
+	{
+		DiaryPage org = DiaryPageUtils.demoPageA();
+		repository.postPage(org);
+		// ------------------
+		DiaryPage restored = repository.getPage(org.getDate());
+		DiaryPageUtils.comparePages(org, restored);
+	}
+
+	public void testPersistanceMultiple()
+	{
+		DiaryPage orgA = DiaryPageUtils.demoPageA();
+		DiaryPage orgB = DiaryPageUtils.demoPageB();
+		List<DiaryPage> pages = new ArrayList<DiaryPage>();
+		pages.add(orgA);
+		pages.add(orgB);
+		repository.postPages(pages);
+
+		List<Date> dates = new ArrayList<Date>();
+		for (DiaryPage page : pages)
+		{
+			dates.add(page.getDate());
+		}
+		// ------------------
+		List<DiaryPage> restored = repository.getPages(dates);
+		assertEquals(pages.size(), restored.size());
+		for (int i = 0; i < pages.size(); i++)
+		{
+			DiaryPageUtils.comparePages(pages.get(i), restored.get(i));
+		}
+	}
+
+	public void testGetModList()
+	{
+		// fail("Not yet implemented"); // TODO
+	}
+}
