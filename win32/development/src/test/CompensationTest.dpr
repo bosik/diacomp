6b4fa542d0825683e11120c986656ec7d28b9784
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-11-24 16:53:33 +0000

    File structure updated: persistence classes moved to appropriative  folder.

--- win32/development/src/test/CompensationTest.dpr
+++ win32/development/src/test/CompensationTest.dpr
@@ -11,16 +11,16 @@ uses
   Bases in '..\bo\Bases.pas',
   AutoLog in '..\common\AutoLog.pas',
   DiaryRoutinesTest in 'DiaryRoutinesTest.pas',
-  DiaryWeb in '..\common\DiaryWeb.pas',
-  DiarySources in '..\common\DiarySources.pas',
+  DiaryWeb in '..\persistence\web\DiaryWeb.pas',
+  DiarySources in '..\persistence\DiarySources.pas',
   DiaryPage in '..\bo\DiaryPage.pas',
   DiaryRecords in '..\bo\DiaryRecords.pas',
-  DiaryPageSerializer in '..\common\DiaryPageSerializer.pas',
+  DiaryPageSerializer in '..\persistence\DiaryPageSerializer.pas',
   DiaryLocalSourceTest in 'DiaryLocalSourceTest.pas',
-  DiaryLocalSource in '..\common\DiaryLocalSource.pas',
+  DiaryLocalSource in '..\persistence\local\DiaryLocalSource.pas',
   DiarySourceTest in 'DiarySourceTest.pas',
   DiaryWebSourceTest in 'DiaryWebSourceTest.pas',
-  DiaryWebSource in '..\common\DiaryWebSource.pas',
+  DiaryWebSource in '..\persistence\web\DiaryWebSource.pas',
   DiaryPageSerializerTest in 'DiaryPageSerializerTest.pas';
 
 {$R *.res}
