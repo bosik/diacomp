a03cf6576f69d91aaa53ba2f89c098188d9629b6
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-12-01 20:24:35 +0000

    Clean up.

--- trunk/android/Comp/src/org/bosik/compensation/face/activities/ActivityMeal.java
+++ trunk/android/Comp/src/org/bosik/compensation/face/activities/ActivityMeal.java
@@ -1,6 +1,7 @@
 package org.bosik.compensation.face.activities;
 
 import java.text.DecimalFormat;
+import java.util.List;
 import org.bosik.compensation.bo.common.FoodMassed;
 import org.bosik.compensation.bo.diary.records.MealRecord;
 import org.bosik.compensation.bo.foodbase.FoodItem;
@@ -23,32 +24,17 @@ import android.widget.TextView;
 public class ActivityMeal extends Activity
 {
 	// отладочная печать
-	private static final String			TAG			= ActivityMeal.class.getSimpleName();
+	private static final String			TAG		= ActivityMeal.class.getSimpleName();
 
-	private static final DecimalFormat	df			= new DecimalFormat("###.#");
+	private static final DecimalFormat	df		= new DecimalFormat("###.#");
 
 	// переменные
-	private static MealRecord			meal		= new MealRecord();
-	private String						lv_arr[]	= {/*
-														 * "Суп сырный (310)",
-														 * "Хлеб черный \"Премиум\" (47)",
-														 * "Фасоль тушеная (249)",
-														 * "Молоко \"Вкуснотеево\", 3,2% (41)",
-														 * "Печенье \"Юбилейное\" молочное (25)",
-														 * "Сыр \"Король Артур\" (40)",
-														 * "Суп сырный (310)",
-														 * "Хлеб черный \"Премиум\" (47)",
-														 * "Фасоль тушеная (249)",
-														 * "Молоко \"Вкуснотеево\", 3,2% (41)",
-														 * "Печенье \"Юбилейное\" молочное (25)",
-														 * "Сыр \"Король Артур\" (40)"
-														 */};
+	private static MealRecord			meal	= new MealRecord();
 
 	// компоненты
 	private AutoCompleteTextView		editName;
 	private EditText					editMass;
 	private Button						buttonAdd;
-	private Activity					activityMeal;
 	private ListView					list;
 	private TextView					textMealCarbs;
 	private TextView					textMealDose;
@@ -61,7 +47,6 @@ public class ActivityMeal extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.editor_meal);
-		activityMeal = this;
 
 		// компоненты
 		editName = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
@@ -115,7 +100,19 @@ public class ActivityMeal extends Activity
 		});
 
 		// назначаем обработчики
-		buttonAdd.setOnClickListener(clickListener);
+		buttonAdd.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				switch (v.getId())
+				{
+					case R.id.button_additem:
+						addItem();
+						break;
+				}
+			}
+		});
 	}
 
 	private void loadFoodList()
@@ -139,15 +136,13 @@ public class ActivityMeal extends Activity
 
 	private void showMeal()
 	{
-		// lv_arr = mealList.toArray(new String[0]);
-		lv_arr = new String[meal.count()];
+		String[] temp = new String[meal.count()];
 		for (int i = 0; i < meal.count(); i++)
 		{
-			lv_arr[i] = printFoodMassed(meal.get(i));
+			temp[i] = printFoodMassed(meal.get(i));
 		}
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(activityMeal, android.R.layout.simple_list_item_1,
-				lv_arr);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, temp);
 		list.setAdapter(adapter);
 
 		double dose = meal.getCarbs() * 0.155;
@@ -160,53 +155,29 @@ public class ActivityMeal extends Activity
 		String name = editName.getText().toString();
 		double mass = Double.parseDouble(editMass.getText().toString());
 
-		FoodItem food = null;
-		FoodMassed item = null;
+		List<FoodItem> list = Storage.localFoodBase.findAny(name);
 
-		// TODO: create generics-based search
-		for (int i = 0; i < Storage.foodBase.size(); i++)
+		if (!list.isEmpty())
 		{
-			if (Storage.foodBase.get(i).getName().equalsIgnoreCase(name))
-			{
-				food = Storage.foodBase.get(i);
+			FoodItem food = list.get(0);
 
-				item = new FoodMassed();
-				item.setName(food.getName());
-				item.setRelProts(food.getRelProts());
-				item.setRelFats(food.getRelFats());
-				item.setRelCarbs(food.getRelCarbs());
-				item.setRelValue(food.getRelValue());
-				item.setMass(mass);
+			FoodMassed item = new FoodMassed();
+			item.setName(food.getName());
+			item.setRelProts(food.getRelProts());
+			item.setRelFats(food.getRelFats());
+			item.setRelCarbs(food.getRelCarbs());
+			item.setRelValue(food.getRelValue());
+			item.setMass(mass);
 
-				meal.add(item);
+			meal.add(item);
 
-				break;
-			}
-		}
+			showMeal();
 
-		showMeal();
-
-		if (item != null)
-		{
-			Log.e("XXX", "ADDED: " + item);
+			Log.v("XXX", "ADDED: " + item);
 			editMass.setText("");
 			editName.setText("");
-			Log.d(TAG, "Moving focus to name field");
+			Log.v(TAG, "Moving focus to name field");
 			editName.requestFocus();
 		}
 	}
-
-	private OnClickListener	clickListener	= new OnClickListener()
-											{
-												@Override
-												public void onClick(View v)
-												{
-													switch (v.getId())
-													{
-														case R.id.button_additem:
-															addItem();
-															break;
-													}
-												}
-											};
 }
