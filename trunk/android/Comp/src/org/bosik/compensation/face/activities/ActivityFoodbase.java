6e913054766da7513609d93ff7a70433563ac9e3
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-12-01 21:02:15 +0000

    Subtext info added.

--- trunk/android/Comp/src/org/bosik/compensation/face/activities/ActivityFoodbase.java
+++ trunk/android/Comp/src/org/bosik/compensation/face/activities/ActivityFoodbase.java
@@ -1,6 +1,7 @@
 package org.bosik.compensation.face.activities;
 
 import java.util.List;
+import java.util.Locale;
 import org.bosik.compensation.bo.foodbase.FoodItem;
 import org.bosik.compensation.face.R;
 import org.bosik.compensation.persistence.Storage;
@@ -8,8 +9,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class ActivityFoodbase extends Activity
 {
@@ -40,7 +44,7 @@ public class ActivityFoodbase extends Activity
 		return true;
 	}
 
-	private void showBase(List<FoodItem> base)
+	private void showBase(final List<FoodItem> base)
 	{
 		String[] str = new String[base.size()];
 		for (int i = 0; i < base.size(); i++)
@@ -48,11 +52,40 @@ public class ActivityFoodbase extends Activity
 			str[i] = base.get(i).getName();
 		}
 
-		Log.e(TAG, "Food base total items: " + str.length);
+		Log.e(TAG, "Food base total items: " + base.size());
 
-		setTitle(String.format("%s (%d", getString(R.string.title_activity_foodbase), str.length));
+		setTitle(String.format("%s (%d)", getString(R.string.title_activity_foodbase), base.size()));
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, str);
+		// ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		// android.R.layout.simple_list_item_1, str);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2,
+				android.R.id.text1, str)
+		{
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent)
+			{
+				View view = super.getView(position, convertView, parent);
+				TextView text1 = (TextView) view.findViewById(android.R.id.text1);
+				TextView text2 = (TextView) view.findViewById(android.R.id.text2);
+
+				text1.setText(base.get(position).getName());
+				text2.setText(getInfo(base.get(position)));
+				return view;
+			}
+		};
+
 		list.setAdapter(adapter);
+	}
+
+	private String getInfo(FoodItem foodItem)
+	{
+		final String p = "Б";
+		final String f = "Ж";
+		final String c = "У";
+		final String v = "К";
+
+		return String.format(Locale.US, "%s %.1f   %s %.1f   %s %.1f   %s %.1f", p, foodItem.getRelProts(), f,
+				foodItem.getRelFats(), c, foodItem.getRelCarbs(), v, foodItem.getRelValue());
 	}
 }
