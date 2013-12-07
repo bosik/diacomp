7d14ca14b0188588a8d32ecf80678fc8712b69ee
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-12-07 18:06:29 +0000

    DishDAO stubs introduced and loaded.

--- trunk/android/Comp/src/org/bosik/compensation/persistence/Storage.java
+++ trunk/android/Comp/src/org/bosik/compensation/persistence/Storage.java
@@ -1,17 +1,20 @@
 package org.bosik.compensation.persistence;
 
 import java.io.IOException;
-import java.util.List;
-import org.bosik.compensation.bo.foodbase.FoodItem;
 import org.bosik.compensation.face.activities.ActivityPreferences;
 import org.bosik.compensation.persistence.dao.DiaryDAO;
+import org.bosik.compensation.persistence.dao.DishBaseDAO;
 import org.bosik.compensation.persistence.dao.FoodBaseDAO;
 import org.bosik.compensation.persistence.dao.local.LocalDiaryDAO;
+import org.bosik.compensation.persistence.dao.local.LocalDishBaseDAO;
 import org.bosik.compensation.persistence.dao.local.LocalFoodBaseDAO;
 import org.bosik.compensation.persistence.dao.web.WebDiaryDAO;
+import org.bosik.compensation.persistence.dao.web.WebDishBaseDAO;
 import org.bosik.compensation.persistence.dao.web.WebFoodBaseDAO;
 import org.bosik.compensation.persistence.dao.web.utils.client.WebClient;
+import org.bosik.compensation.persistence.serializers.dishbase.DishBaseXMLSerializer;
 import org.bosik.compensation.persistence.serializers.foodbase.FoodBaseXMLSerializer;
+import org.bosik.compensation.services.RelevantIndexator;
 import org.bosik.compensation.utils.ErrorHandler;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -25,21 +28,24 @@ import android.util.Log;
  */
 public class Storage
 {
-	private static final String		TAG					= Storage.class.getSimpleName();
+	private static final String	TAG					= Storage.class.getSimpleName();
 
-	private static final String		FILENAME_FOODBASE	= "FoodBase.xml";
-	// private static final String FILENAME_DISHBASE = "DishBase.xml";
-	private static final int		CONNECTION_TIMEOUT	= 6000;
+	private static final String	FILENAME_FOODBASE	= "FoodBase.xml";
+	private static final String	FILENAME_DISHBASE	= "DishBase.xml";
+	private static final int	CONNECTION_TIMEOUT	= 6000;
 
 	// DAO
-	public static WebClient			webClient;
-	public static DiaryDAO			localDiary;
-	public static DiaryDAO			webDiary;
-	public static FoodBaseDAO		localFoodBase;
-	public static FoodBaseDAO		webFoodBase;
 
-	// temp data
-	public static List<FoodItem>	foodBase;
+	public static WebClient		webClient;
+
+	public static DiaryDAO		localDiary;
+	public static DiaryDAO		webDiary;
+
+	public static FoodBaseDAO	localFoodBase;
+	public static FoodBaseDAO	webFoodBase;
+
+	public static DishBaseDAO	localDishBase;
+	public static DishBaseDAO	webDishBase;
 
 	/**
 	 * Initializes the storage. Might be called sequentially
@@ -71,28 +77,48 @@ public class Storage
 		}
 		if (null == localFoodBase)
 		{
-			Log.v(TAG, "Local foodbase initialization...");
+			Log.v(TAG, "Local food base initialization...");
 			try
 			{
 				localFoodBase = new LocalFoodBaseDAO(context, FILENAME_FOODBASE);
-				foodBase = localFoodBase.findAll();
 			}
 			catch (IOException e)
 			{
 				localFoodBase = null;
-				throw new RuntimeException("Failed to create local base DAO", e);
+				throw new RuntimeException("Failed to create local food base DAO", e);
 			}
 		}
-		if (null == Storage.webFoodBase)
+		if (null == localDishBase)
 		{
-			Log.v(TAG, "Web foodbase initialization...");
+			Log.v(TAG, "Local dish base initialization...");
+			try
+			{
+				localDishBase = new LocalDishBaseDAO(context, FILENAME_DISHBASE);
+			}
+			catch (IOException e)
+			{
+				localDishBase = null;
+				throw new RuntimeException("Failed to create local dish base DAO", e);
+			}
+		}
+		if (null == webFoodBase)
+		{
+			Log.v(TAG, "Web food base initialization...");
 			webFoodBase = new WebFoodBaseDAO(webClient, new FoodBaseXMLSerializer());
+		}
+		if (null == webDishBase)
+		{
+			Log.v(TAG, "Web dish base initialization...");
+			webDishBase = new WebDishBaseDAO(webClient, new DishBaseXMLSerializer());
 		}
 
 		ErrorHandler.init(webClient);
 
 		// this applies all preferences
 		applyPreference(preferences, null);
+
+		// analyze using
+		RelevantIndexator.indexate(localDiary, localFoodBase, localDishBase);
 	}
 
 	private static boolean check(String testKey, String baseKey)
