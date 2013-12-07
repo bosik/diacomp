2f153192e6bcb0089feaa91642ec54140bbe42ac
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-12-07 15:08:57 +0000

    RelevantIndexator service introduced.

---
+++ trunk/android/Comp/src/org/bosik/compensation/services/RelevantIndexator.java
@@ -0,0 +1,82 @@
+package org.bosik.compensation.services;
+
+import java.util.Date;
+import java.util.List;
+import org.bosik.compensation.bo.FoodMassed;
+import org.bosik.compensation.bo.RelativeTagged;
+import org.bosik.compensation.bo.diary.DiaryPage;
+import org.bosik.compensation.bo.diary.records.DiaryRecord;
+import org.bosik.compensation.bo.diary.records.MealRecord;
+import org.bosik.compensation.persistence.dao.BaseDAO;
+import org.bosik.compensation.persistence.dao.DiaryDAO;
+import org.bosik.compensation.persistence.dao.DishBaseDAO;
+import org.bosik.compensation.persistence.dao.FoodBaseDAO;
+import org.bosik.compensation.utils.Utils;
+
+public class RelevantIndexator
+{
+	public static void indexate(DiaryDAO diary, FoodBaseDAO foodBase, DishBaseDAO dishBase)
+	{
+		// constructing dates list
+		final Date now = Utils.now();
+		final int PERIOD = 30; // days
+		final List<Date> dates = Utils.getPeriodDates(now, PERIOD);
+
+		// clear tags
+		clearTags(foodBase);
+		clearTags(dishBase);
+
+		// process
+		List<DiaryPage> pages = diary.getPages(dates);
+		for (DiaryPage page : pages)
+		{
+			for (int i = 0; i < page.count(); i++)
+			{
+				DiaryRecord rec = page.get(i);
+				if (rec.getClass().equals(MealRecord.class))
+				{
+					MealRecord meal = (MealRecord) rec;
+					for (int j = 0; j < meal.count(); j++)
+					{
+						FoodMassed food = meal.get(j);
+						process(food.getName(), foodBase, dishBase);
+					}
+				}
+			}
+		}
+	}
+
+	private static <T extends RelativeTagged> void clearTags(BaseDAO<T> base)
+	{
+		List<T> list = base.findAll();
+		for (RelativeTagged item : list)
+		{
+			item.setTag(0);
+		}
+		base.replaceAll(list, base.getVersion());
+	}
+
+	private static void process(String name, FoodBaseDAO foodBase, DishBaseDAO dishBase)
+	{
+		if (process(name, foodBase))
+			return;
+		if (process(name, dishBase))
+			return;
+	}
+
+	@SuppressWarnings("unchecked")
+	private static <T extends RelativeTagged> boolean process(String name, BaseDAO<T> base)
+	{
+		RelativeTagged item = base.findOne(name);
+		if (null != item)
+		{
+			item.setTag(item.getTag() + 1);
+			base.update((T) item);
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
