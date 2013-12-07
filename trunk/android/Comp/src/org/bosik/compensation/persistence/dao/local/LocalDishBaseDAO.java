bc2ce51c491f8c4ce8522a213e5fba05624f3d9d
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-12-07 11:11:53 +0000

    DishBaseDAO introduced.

--- trunk/android/Comp/src/org/bosik/compensation/persistence/dao/local/LocalDishBaseDAO.java
+++ trunk/android/Comp/src/org/bosik/compensation/persistence/dao/local/LocalDishBaseDAO.java
@@ -2,11 +2,12 @@ package org.bosik.compensation.persistence.dao.local;
 
 import java.io.IOException;
 import org.bosik.compensation.bo.dishbase.DishItem;
+import org.bosik.compensation.persistence.dao.DishBaseDAO;
 import org.bosik.compensation.persistence.dao.local.utils.FileBaseDAO;
 import org.bosik.compensation.persistence.serializers.dishbase.DishBaseXMLSerializer;
 import android.content.Context;
 
-public class LocalDishBaseDAO extends FileBaseDAO<DishItem>
+public class LocalDishBaseDAO extends FileBaseDAO<DishItem> implements DishBaseDAO
 {
 	public LocalDishBaseDAO(Context context, String fileName) throws IOException
 	{
