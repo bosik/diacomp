4d402ef7aa0de799df8ddcef9037fec9ff5221c7
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-12-14 23:12:52 +0000

    Fake tests ignored.

--- trunk/android/CompTest/src/org/bosik/compensation/persistence/dao/fakes/FakeFoodBaseDAOTest.java
+++ trunk/android/CompTest/src/org/bosik/compensation/persistence/dao/fakes/FakeFoodBaseDAOTest.java
@@ -3,7 +3,9 @@ package org.bosik.compensation.persistence.dao.fakes;
 import org.bosik.compensation.fakes.dao.FakeFoodBaseDAO;
 import org.bosik.compensation.persistence.dao.FoodBaseDAO;
 import org.bosik.compensation.persistence.dao.FoodBaseDAOTest;
+import android.test.suitebuilder.annotation.Suppress;
 
+@Suppress
 public class FakeFoodBaseDAOTest extends FoodBaseDAOTest
 {
 	@Override
