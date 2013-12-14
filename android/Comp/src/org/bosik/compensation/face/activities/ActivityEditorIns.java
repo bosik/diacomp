ead2bcd65a3b1aa8e4895a250665b0ce9b9c9c48
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-12-14 18:54:11 +0000

    Ins editor implemented.

--- android/Comp/src/org/bosik/compensation/face/activities/ActivityEditorIns.java
+++ android/Comp/src/org/bosik/compensation/face/activities/ActivityEditorIns.java
@@ -2,25 +2,92 @@ package org.bosik.compensation.face.activities;
 
 import org.bosik.compensation.bo.diary.records.InsRecord;
 import org.bosik.compensation.face.R;
+import org.bosik.compensation.face.UIUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class ActivityEditorIns extends ActivityEditor<InsRecord>
 {
+	// components
+	private TimePicker	timePicker;
+	private DatePicker	datePicker;
+	private EditText	editValue;
+	private Button		buttonOK;
+
 	@Override
 	protected void setupInterface()
 	{
 		setContentView(R.layout.editor_ins);
+		timePicker = (TimePicker) findViewById(R.id.pickerInsTime);
+		timePicker.setIs24HourView(true);
+		datePicker = (DatePicker) findViewById(R.id.pickerInsDate);
+		editValue = (EditText) findViewById(R.id.editInsValue);
+		buttonOK = (Button) findViewById(R.id.buttonInsOK);
+		buttonOK.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				ActivityEditorIns.this.submit();
+			}
+		});
 	}
 
 	@Override
 	protected void showValuesInGUI(boolean createMode)
 	{
-		// TODO Auto-generated method stub
+		timePicker.setCurrentHour(entity.getTime() / 60);
+		timePicker.setCurrentMinute(entity.getTime() % 60);
+
+		if (!createMode)
+		{
+			editValue.setText(String.valueOf(entity.getValue()));
+		}
+		else
+		{
+			editValue.setText("");
+		}
 	}
 
 	@Override
 	protected boolean getValuesFromGUI()
 	{
-		// TODO Auto-generated method stub
+		// TODO: localize error messages
+
+		// time
+		try
+		{
+			entity.setTime((timePicker.getCurrentHour() * 60) + timePicker.getCurrentMinute());
+		}
+		catch (IllegalArgumentException e)
+		{
+			UIUtils.showTip(this, "Введите корректное время");
+			timePicker.requestFocus();
+			return false;
+		}
+
+		// value
+		try
+		{
+			entity.setValue(Double.parseDouble(editValue.getText().toString()));
+		}
+		catch (NumberFormatException e)
+		{
+			UIUtils.showTip(this, "Введите корректное значение инъекции");
+			editValue.requestFocus();
+			return false;
+		}
+		catch (IllegalArgumentException e)
+		{
+			UIUtils.showTip(this, "Введите корректное значение инъекции");
+			editValue.requestFocus();
+			return false;
+		}
+
 		return true;
 	}
 }
