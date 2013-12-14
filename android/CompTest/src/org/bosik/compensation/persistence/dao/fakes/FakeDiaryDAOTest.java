b610445dd46df62aabc595779bfc0a1256900f38
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-12-14 23:12:52 +0000

    Fake tests ignored.

--- android/CompTest/src/org/bosik/compensation/persistence/dao/fakes/FakeDiaryDAOTest.java
+++ android/CompTest/src/org/bosik/compensation/persistence/dao/fakes/FakeDiaryDAOTest.java
@@ -3,7 +3,9 @@ package org.bosik.compensation.persistence.dao.fakes;
 import org.bosik.compensation.fakes.dao.FakeDiaryDAO;
 import org.bosik.compensation.persistence.dao.DiaryDAO;
 import org.bosik.compensation.persistence.dao.DiaryDAOTest;
+import android.test.suitebuilder.annotation.Suppress;
 
+@Suppress
 public class FakeDiaryDAOTest extends DiaryDAOTest
 {
 	private static DiaryDAO	dao	= new FakeDiaryDAO();
