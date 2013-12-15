c2a1ed3284b5498ecf7633522fc12d472df9f634
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-12-15 12:21:18 +0000

    Clean-up.

--- trunk/android/Comp/src/org/bosik/compensation/bo/basic/Named.java
+++ trunk/android/Comp/src/org/bosik/compensation/bo/basic/Named.java
@@ -2,43 +2,5 @@ package org.bosik.compensation.bo.basic;
 
 public interface Named
 {
-	// private String name;
-
-	// public Named(String name)
-	// {
-	// setName(name);
-	// }
-
 	String getName();
-
-	// ================================ GET / SET ================================
-
-	// public String getName()
-	// {
-	// return name;
-	// }
-	//
-	// public void setName(String name)
-	// {
-	// if (name == null)
-	// {
-	// throw new NullPointerException("Name can't be null");
-	// }
-	// if (name.trim().equals(""))
-	// {
-	// throw new IllegalArgumentException("Name must contain non-whitespace characters");
-	// }
-	//
-	// this.name = name;
-	// }
-
-	// ================================ CLONE ================================
-
-	// @Override
-	// public Named clone() throws CloneNotSupportedException
-	// {
-	// Named result = (Named) super.clone();
-	// result.name = name;
-	// return result;
-	// }
 }
