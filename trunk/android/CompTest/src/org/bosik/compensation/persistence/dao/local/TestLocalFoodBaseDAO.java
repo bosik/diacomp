86a7c9717e21d06bd939d97afd5292a22b8e1b60
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-12-21 09:32:50 +0000

    Old foodbase tests ignored.

--- trunk/android/CompTest/src/org/bosik/compensation/persistence/dao/local/TestLocalFoodBaseDAO.java
+++ trunk/android/CompTest/src/org/bosik/compensation/persistence/dao/local/TestLocalFoodBaseDAO.java
@@ -2,7 +2,9 @@ package org.bosik.compensation.persistence.dao.local;
 
 import org.bosik.compensation.persistence.dao.FoodBaseDAO;
 import org.bosik.compensation.persistence.dao.TestFoodBaseDAO;
+import android.test.suitebuilder.annotation.Suppress;
 
+@Suppress
 public class TestLocalFoodBaseDAO extends TestFoodBaseDAO
 {
 	@Override
