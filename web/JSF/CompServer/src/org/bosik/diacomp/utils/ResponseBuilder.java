ea649a5de9a89a1bcf45765b18a27b4104e13507
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2013-12-28 23:59:37 +0000

    JSON response builder introduced. Error handling added.

---
+++ web/JSF/CompServer/src/org/bosik/diacomp/utils/ResponseBuilder.java
@@ -0,0 +1,36 @@
+package org.bosik.diacomp.utils;
+
+public class ResponseBuilder
+{
+	public static final int	CODE_OK				= 0;
+	public static final int	CODE_NOTFOUND		= 404;
+	public static final int	CODE_FAIL			= 500;
+	public static final int	CODE_BADCREDENTIALS	= 2;	// TODO
+	public static final int	CODE_UNAUTHORIZED	= 401;
+
+	public static String build(int code, String msg)
+	{
+		// StringEscapeUtils.escapeJava(str)
+		if (msg != null)
+		{
+			msg = msg.replace("\"", "\\\"");
+		}
+
+		return String.format("{code:%d, response:\"%s\"}", code, msg);
+	}
+
+	public static String buildDone(String msg)
+	{
+		return build(CODE_OK, msg);
+	}
+
+	public static String buildFails(String msg)
+	{
+		return build(CODE_FAIL, msg);
+	}
+
+	public static String buildNotAuthorized()
+	{
+		return build(CODE_UNAUTHORIZED, "Not authorized");
+	}
+}
