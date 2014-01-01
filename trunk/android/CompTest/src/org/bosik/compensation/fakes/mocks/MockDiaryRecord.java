f8d2f2a711851deaca2738878e4fca71f5da06db
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-01-01 23:52:20 +0000

    Warnings fixed.

--- trunk/android/CompTest/src/org/bosik/compensation/fakes/mocks/MockDiaryRecord.java
+++ trunk/android/CompTest/src/org/bosik/compensation/fakes/mocks/MockDiaryRecord.java
@@ -1,7 +1,6 @@
 package org.bosik.compensation.fakes.mocks;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import junit.framework.TestCase;
 import org.bosik.compensation.bo.FoodMassed;
@@ -20,33 +19,33 @@ public class MockDiaryRecord extends TestCase implements Mock<DiaryRecord>
 	{
 		List<DiaryRecord> samples = new ArrayList<DiaryRecord>();
 
-		samples.add(new BloodRecord(new Date(2012, 04, 18, 16, 42, 05), 7.1, 0));
-		samples.add(new BloodRecord(new Date(2012, 04, 18, 16, 50, 28), 7.0, 1));
-		samples.add(new BloodRecord(new Date(2012, 04, 18, 17, 00, 00), 5.2, 2));
+		samples.add(new BloodRecord(TestUtils.time(2012, 04, 18, 16, 42, 05), 7.1, 0));
+		samples.add(new BloodRecord(TestUtils.time(2012, 04, 18, 16, 50, 28), 7.0, 1));
+		samples.add(new BloodRecord(TestUtils.time(2012, 04, 18, 17, 00, 00), 5.2, 2));
 
-		samples.add(new InsRecord(new Date(2012, 04, 18, 17, 20, 00), 16.0));
+		samples.add(new InsRecord(TestUtils.time(2012, 04, 18, 17, 20, 00), 16.0));
 
-		MealRecord meal1 = new MealRecord(new Date(2012, 04, 18, 17, 25, 59), false);
+		MealRecord meal1 = new MealRecord(TestUtils.time(2012, 04, 18, 17, 25, 59), false);
 		meal1.add(new FoodMassed("Карбонат \"Восточный\" (Черн)", 9.9, 26.3, 0, 276, 90));
 		meal1.add(new FoodMassed("Хлеб чёрный \"Премиум\"", 5.5, 0.9, 44.1, 206.3, 42));
 		samples.add(meal1);
 
-		MealRecord meal2 = new MealRecord(new Date(2012, 04, 18, 23, 59, 59), true);
+		MealRecord meal2 = new MealRecord(TestUtils.time(2012, 04, 18, 23, 59, 59), true);
 		meal2.add(new FoodMassed("Сахар", 0.0, 0.0, 99.8, 379.0, 6.0));
 		samples.add(meal2);
 
-		MealRecord meal3 = new MealRecord(new Date(2012, 04, 18, 23, 59, 59), true);
+		MealRecord meal3 = new MealRecord(TestUtils.time(2012, 04, 18, 23, 59, 59), true);
 		for (FoodMassed f : mockFoodMassed.getSamples())
 		{
 			meal3.add(f);
 		}
 		samples.add(meal3);
 
-		samples.add(new MealRecord(new Date(2012, 04, 18, 0, 0, 0), true));
+		samples.add(new MealRecord(TestUtils.time(2012, 04, 18, 0, 0, 0), true));
 
-		samples.add(new NoteRecord(new Date(2014, 01, 01, 12, 00, 00), "Just a тестовая record with \"quotes\""));
+		samples.add(new NoteRecord(TestUtils.time(2014, 01, 01, 12, 00, 00), "Just a тестовая record with \"quotes\""));
 
-		samples.add(new NoteRecord(new Date(2013, 12, 31, 23, 59, 59), ""));
+		samples.add(new NoteRecord(TestUtils.time(2013, 12, 31, 23, 59, 59), ""));
 
 		return samples;
 	}
@@ -64,13 +63,13 @@ public class MockDiaryRecord extends TestCase implements Mock<DiaryRecord>
 		// @formatter:on
 	}
 
-	private void compareBloodRecords(BloodRecord exp, BloodRecord act)
+	private static void compareBloodRecords(BloodRecord exp, BloodRecord act)
 	{
 		assertEquals(exp.getValue(), act.getValue(), TestUtils.EPS);
 		assertEquals(exp.getFinger(), act.getFinger());
 	}
 
-	private void compareInsRecords(InsRecord exp, InsRecord act)
+	private static void compareInsRecords(InsRecord exp, InsRecord act)
 	{
 		assertEquals(exp.getValue(), act.getValue(), TestUtils.EPS);
 	}
