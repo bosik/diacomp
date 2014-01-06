af95ad9290447b26720197cf5039221402e66a7b
Админ <@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-01-06 18:42:40 +0000

    Dependency test.

--- web/JSF/CompServer/src/main/java/org/bosik/diacomp/resources/DiaryResource.java
+++ web/JSF/CompServer/src/main/java/org/bosik/diacomp/resources/DiaryResource.java
@@ -12,63 +12,60 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import org.bosik.compensation.persistence.common.Versioned;
-import org.bosik.compensation.persistence.serializers.Parser;
-import org.bosik.compensation.persistence.serializers.Serializer;
-import org.bosik.compensation.persistence.serializers.utils.ParserVersioned;
-import org.bosik.compensation.persistence.serializers.utils.SerializerAdapter;
+import org.bosik.diacomp.Demo;
 import org.bosik.diacomp.services.DiaryService;
 import org.bosik.diacomp.utils.ResponseBuilder;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 @Path("diary")
 public class DiaryResource
 {
 	@Context
-	HttpServletRequest									req;
+	HttpServletRequest		req;
 
-	private DiaryService								diaryService				= new DiaryService();
+	private DiaryService	diaryService	= new DiaryService();
 
-	private static final Parser<String>					parserString				= new Parser<String>()
-																					{
-																						// As-is
-																						// "parser"
-
-																						@Override
-																						public String read(
-																								JSONObject json)
-																								throws JSONException
-																						{
-																							if (json == null)
-																							{
-																								return null;
-																							}
-																							else
-																							{
-																								return json.toString();
-																							}
-																						}
-
-																						@Override
-																						public JSONObject write(
-																								String object)
-																								throws JSONException
-																						{
-																							if (object == null)
-																							{
-																								return null;
-																							}
-																							else
-																							{
-																								return new JSONObject(
-																										object);
-																							}
-																						}
-																					};
-	private static final Parser<Versioned<String>>		parserVersionedString		= new ParserVersioned<String>(
-																							parserString);
-	private static final Serializer<Versioned<String>>	serializerVersionedString	= new SerializerAdapter<Versioned<String>>(
-																							parserVersionedString);
+	// private static final Parser<String> parserString = new Parser<String>()
+	// {
+	// // As-is
+	// // "parser"
+	//
+	// @Override
+	// public String read(
+	// JSONObject json)
+	// throws JSONException
+	// {
+	// if (json == null)
+	// {
+	// return null;
+	// }
+	// else
+	// {
+	// return json.toString();
+	// }
+	// }
+	//
+	// @Override
+	// public JSONObject write(
+	// String object)
+	// throws JSONException
+	// {
+	// if (object == null)
+	// {
+	// return null;
+	// }
+	// else
+	// {
+	// return new JSONObject(
+	// object);
+	// }
+	// }
+	// };
+	// private static final Parser<Versioned<String>> parserVersionedString = new
+	// ParserVersioned<String>(
+	// parserString);
+	// private static final Serializer<Versioned<String>> serializerVersionedString = new
+	// SerializerAdapter<Versioned<String>>(
+	// parserVersionedString);
 
 	@GET
 	@Path("/view")
@@ -79,7 +76,10 @@ public class DiaryResource
 		{
 			int userId = AuthResource.getCurrentUserId(req);
 			List<Versioned<String>> list = diaryService.findAll(userId);
-			return serializerVersionedString.writeAll(list);
+			// return serializerVersionedString.writeAll(list);
+
+			Demo demo = new Demo();
+			return String.valueOf(demo.test(2, 3));
 		}
 		catch (Exception e)
 		{
