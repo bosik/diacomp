894e23d288d4ca136dc078f91c90ae31e49efe1c
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-01-11 22:33:29 +0000

    [Unstable] Fake DAO tests moved to transfer project.

--- trunk/web/JSF/CompTransfer/src/test/java/org/bosik/compensation/persistence/dao/fakes/TestFakeDiaryDAO.java
+++ trunk/web/JSF/CompTransfer/src/test/java/org/bosik/compensation/persistence/dao/fakes/TestFakeDiaryDAO.java
@@ -2,10 +2,9 @@ package org.bosik.compensation.persistence.dao.fakes;
 
 import org.bosik.compensation.fakes.dao.FakeDiaryDAO;
 import org.bosik.compensation.persistence.dao.DiaryDAO;
-import org.bosik.compensation.persistence.dao.TestDiaryDAO;
-import android.test.suitebuilder.annotation.Suppress;
+import org.junit.Ignore;
 
-@Suppress
+@Ignore
 public class TestFakeDiaryDAO extends TestDiaryDAO
 {
 	private static DiaryDAO	dao	= new FakeDiaryDAO();
