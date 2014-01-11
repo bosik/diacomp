1b638cd70be3f94ceff516ac70f3cb2ddff4c76f
Bosik <Bosik@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-01-11 21:54:02 +0000

    Logging removed.

--- trunk/android/CompTest/src/org/bosik/compensation/utills/TestUtils.java
+++ trunk/android/CompTest/src/org/bosik/compensation/utills/TestUtils.java
@@ -6,14 +6,13 @@ import java.util.Date;
 import java.util.TimeZone;
 import junit.framework.TestCase;
 import org.bosik.compensation.utils.AndroidUtils;
-import android.util.Log;
 
 @SuppressWarnings("static-method")
 public class TestUtils extends TestCase
 {
-	private static final String	TAG			= TestUtils.class.getSimpleName();
+	// private static final String TAG = TestUtils.class.getSimpleName();
 	public static final double	EPS			= 0.00001;
-	public static final long	EPS_TIME	= 5000;							// ms
+	public static final long	EPS_TIME	= 5000;	// ms
 
 	public static Date date(int year, int month, int day)
 	{
@@ -32,35 +31,35 @@ public class TestUtils extends TestCase
 		return c.getTime();
 	}
 
-	private static String formatArray(byte array[])
-	{
-		StringBuilder sb = new StringBuilder("{");
+	// private static String formatArray(byte array[])
+	// {
+	// StringBuilder sb = new StringBuilder("{");
+	//
+	// for (int i = 0; i < array.length; i++)
+	// {
+	// sb.append(array[i]);
+	// if (i < (array.length - 1))
+	// {
+	// sb.append(", ");
+	// }
+	// }
+	//
+	// sb.append("}");
+	// return sb.toString();
+	// }
 
-		for (int i = 0; i < array.length; i++)
-		{
-			sb.append(array[i]);
-			if (i < (array.length - 1))
-			{
-				sb.append(", ");
-			}
-		}
-
-		sb.append("}");
-		return sb.toString();
-	}
-
-	public void testEncodingUtf8Cp1251Utf8Persistance() throws UnsupportedEncodingException
+	public void testEncodingUtf8Cp1251Utf8Persistance()
 	{
 		String utf8 = "моя строка";
 		String cp1251 = AndroidUtils.Utf8ToCp1251(utf8);
 		String utf8_again = AndroidUtils.Cp1251ToUtf8(cp1251);
 
-		Log.d(TAG, "Pers. test: utf8 = " + utf8);
-		Log.d(TAG, "Pers. test: utf8 = " + formatArray(utf8.getBytes("UTF-8")));
-		Log.d(TAG, "Pers. test: cp1251 = " + cp1251);
-		Log.d(TAG, "Pers. test: cp1251 = " + formatArray(cp1251.getBytes("Cp1251")));
-		Log.d(TAG, "Pers. test: utf8_again = " + utf8_again);
-		Log.d(TAG, "Pers. test: utf8_again = " + formatArray(utf8_again.getBytes("UTF-8")));
+		// Log.d(TAG, "Pers. test: utf8 = " + utf8);
+		// Log.d(TAG, "Pers. test: utf8 = " + formatArray(utf8.getBytes("UTF-8")));
+		// Log.d(TAG, "Pers. test: cp1251 = " + cp1251);
+		// Log.d(TAG, "Pers. test: cp1251 = " + formatArray(cp1251.getBytes("Cp1251")));
+		// Log.d(TAG, "Pers. test: utf8_again = " + utf8_again);
+		// Log.d(TAG, "Pers. test: utf8_again = " + formatArray(utf8_again.getBytes("UTF-8")));
 
 		assertEquals(utf8, utf8_again);
 	}
@@ -70,10 +69,10 @@ public class TestUtils extends TestCase
 		String utf8 = "моя строка";
 		String cp1251 = AndroidUtils.Utf8ToCp1251(utf8);
 
-		Log.d(TAG, "8->1251 test: utf8 = " + utf8);
-		Log.d(TAG, "8->1251 test: utf8 = " + formatArray(utf8.getBytes("UTF-8")));
-		Log.d(TAG, "8->1251 test: cp1251 = " + cp1251);
-		Log.d(TAG, "8->1251 test: cp1251 = " + formatArray(cp1251.getBytes("Cp1251")));
+		// Log.d(TAG, "8->1251 test: utf8 = " + utf8);
+		// Log.d(TAG, "8->1251 test: utf8 = " + formatArray(utf8.getBytes("UTF-8")));
+		// Log.d(TAG, "8->1251 test: cp1251 = " + cp1251);
+		// Log.d(TAG, "8->1251 test: cp1251 = " + formatArray(cp1251.getBytes("Cp1251")));
 
 		byte cp1251_bytes[] = cp1251.getBytes("Cp1251");
 		int expected_bytes[] = new int[] { 0xEC, 0xEE, 0xFF, 0x20, 0xF1, 0xF2, 0xF0, 0xEE, 0xEA, 0xE0 };
@@ -113,10 +112,10 @@ public class TestUtils extends TestCase
 			cp1251 = new String(cp1251_bytes, "Cp1251");
 			utf8 = AndroidUtils.Cp1251ToUtf8(cp1251);
 
-			Log.d(TAG, "1251->8 test: cp1251 = " + cp1251);
-			Log.d(TAG, "1251->8 test: cp1251 = " + formatArray(cp1251.getBytes("Cp1251")));
-			Log.d(TAG, "1251->8 test: utf8 = " + utf8);
-			Log.d(TAG, "1251->8 test: utf8 = " + formatArray(utf8.getBytes("UTF-8")));
+			// Log.d(TAG, "1251->8 test: cp1251 = " + cp1251);
+			// Log.d(TAG, "1251->8 test: cp1251 = " + formatArray(cp1251.getBytes("Cp1251")));
+			// Log.d(TAG, "1251->8 test: utf8 = " + utf8);
+			// Log.d(TAG, "1251->8 test: utf8 = " + formatArray(utf8.getBytes("UTF-8")));
 
 		}
 		catch (UnsupportedEncodingException e)
