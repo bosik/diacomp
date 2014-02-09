d5988eceadc9d01ab477542f38899a54cd5d87ca
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-02-09 00:00:28 +0000

    Tests renamed. Access test added.

--- trunk/web/JSF/CompServer/src/test/java/org/bosik/diacomp/frontend/services/TestAuthWebService.java
+++ trunk/web/JSF/CompServer/src/test/java/org/bosik/diacomp/frontend/services/TestAuthWebService.java
@@ -1,6 +1,8 @@
 package org.bosik.diacomp.frontend.services;
 
+import java.util.Date;
 import org.bosik.diacomp.services.AuthService;
+import org.bosik.diacomp.services.DiaryService;
 import org.bosik.diacomp.services.exceptions.DeprecatedAPIException;
 import org.bosik.diacomp.services.exceptions.NotAuthorizedException;
 import org.bosik.diacomp.services.exceptions.UnsupportedAPIException;
@@ -8,45 +10,54 @@ import org.junit.Test;
 
 public class TestAuthWebService
 {
-	private static final String	demoLogin	= "admin";
-	private static final String	demoPass	= "1234";
-	private static final int	apiVersion	= 20;
+	private static final String	demoLogin		= "admin";
+	private static final String	demoPass		= "1234";
+	private static final int	apiVersion		= 20;
 
-	private AuthService			authService	= new AuthWebService();
+	private AuthService			authService		= new AuthWebService();
+	private DiaryService		diaryService	= new DiaryWebService();
 
 	@Test
-	public void loginTest_correct()
+	public void testLogin_correct()
 	{
 		// No exceptions will be thrown if auth is done ok
 		authService.login(demoLogin, demoPass, apiVersion);
+		diaryService.getRecords(new Date());
 	}
 
 	@Test(expected = NotAuthorizedException.class)
-	public void loginTest_wrongPass()
+	public void testLogin_wrongPass()
 	{
 		authService.login(demoLogin, demoPass + "#:&%$'\"/\\", apiVersion);
 	}
 
 	@Test(expected = NotAuthorizedException.class)
-	public void loginTest_wrongLogin()
+	public void testLogin_wrongLogin()
 	{
 		authService.login(demoLogin + "#:&%$'\"/\\", demoPass, apiVersion);
 	}
 
 	@Test(expected = UnsupportedAPIException.class)
-	public void loginTest_unsupportedApi()
+	public void testLogin_unsupportedApi()
 	{
 		authService.login(demoLogin, demoPass, 18);
 	}
 
 	@Test(expected = DeprecatedAPIException.class)
-	public void loginTest_deprecatedApi()
+	public void testLogin_deprecatedApi()
 	{
 		authService.login(demoLogin, demoPass, 19);
 	}
 
-	public void logoutTest()
+	public void testLogout()
 	{
 		authService.logout();
+	}
+
+	@Test(expected = NotAuthorizedException.class)
+	public void testUnauthAccess()
+	{
+		authService.logout();
+		diaryService.getRecords(new Date());
 	}
 }
