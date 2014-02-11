947b9ab093bacb62b5b905ec770ad1e526f1919f
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-02-11 15:24:49 +0000

    ApacheHttpClient used. Cookie problem fixed.

--- trunk/web/JSF/CompServer/src/main/java/org/bosik/diacomp/frontend/services/WebService.java
+++ trunk/web/JSF/CompServer/src/main/java/org/bosik/diacomp/frontend/services/WebService.java
@@ -4,9 +4,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.client.apache.ApacheHttpClient;
+import com.sun.jersey.client.apache.config.ApacheHttpClientConfig;
+import com.sun.jersey.client.apache.config.DefaultApacheHttpClientConfig;
+
 public class WebService
 {
-	private String	BASE_URL;
+	private static ApacheHttpClient	client;
+	private String					BASE_URL;
+
+	{
+		ApacheHttpClientConfig config = new DefaultApacheHttpClientConfig();
+		config.getProperties().put(ApacheHttpClientConfig.PROPERTY_HANDLE_COOKIES, true);
+		client = ApacheHttpClient.create(config);
+	}
 
 	public WebService(String url)
 	{
@@ -21,10 +33,9 @@ public class WebService
 
 			ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 			InputStream is = classloader.getResourceAsStream("config.properties");
-
-			// InputStream is = getClass().getResourceAsStream("config.properties");
 			pro.load(is);
 			BASE_URL = pro.getProperty("baseUrl");
+			is.close();
 
 			System.out.println(getClass().getSimpleName() + " loaded with URL " + BASE_URL);
 		}
@@ -37,5 +48,10 @@ public class WebService
 	public String getBaseUrl()
 	{
 		return BASE_URL;
+	}
+
+	public static Client getClient()
+	{
+		return client;
 	}
 }
