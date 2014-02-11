68c17bb884d50744a54504a71ee08d8c55c83a77
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-02-11 16:21:25 +0000

    [Unstable] FakeDiaryDAO added.

---
+++ web/JSF/CompServer/src/main/java/org/bosik/diacomp/backend/dao/FakeDiaryDAO.java
@@ -0,0 +1,43 @@
+package org.bosik.diacomp.backend.dao;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.bosik.diacomp.bo.diary.DiaryRecord;
+import org.bosik.diacomp.persistence.common.Versioned;
+import org.bosik.diacomp.persistence.mocks.Mock;
+import org.bosik.diacomp.persistence.mocks.MockDiaryRecord;
+import org.bosik.diacomp.persistence.mocks.MockVersionedConverter;
+import org.bosik.diacomp.persistence.serializers.Parser;
+import org.bosik.diacomp.persistence.serializers.ParserDiaryRecord;
+import org.bosik.diacomp.persistence.serializers.Serializer;
+import org.bosik.diacomp.persistence.serializers.utils.SerializerAdapter;
+
+public class FakeDiaryDAO implements IDiaryDAO
+{
+	private static Mock<Versioned<DiaryRecord>>	mock		= new MockVersionedConverter<DiaryRecord>(
+																	new MockDiaryRecord());
+	private static List<Versioned<DiaryRecord>>	samples		= mock.getSamples();
+	private static Parser<DiaryRecord>			parser		= new ParserDiaryRecord();
+	private static Serializer<DiaryRecord>		serializer	= new SerializerAdapter<DiaryRecord>(parser);
+
+	@Override
+	public List<Versioned<String>> findMod(int userId, Date time)
+	{
+		List<Versioned<String>> result = new ArrayList<Versioned<String>>();
+
+		for (Versioned<DiaryRecord> rec : samples)
+		{
+			Versioned<String> item = new Versioned<String>();
+			item.setId(rec.getId());
+			item.setTimeStamp(rec.getTimeStamp());
+			item.setVersion(rec.getVersion());
+			item.setDeleted(rec.isDeleted());
+			item.setData(serializer.write(rec.getData()));
+			result.add(item);
+		}
+
+		return result;
+	}
+}
