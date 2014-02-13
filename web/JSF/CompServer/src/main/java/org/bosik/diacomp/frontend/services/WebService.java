53521729b9f2f3f8a150cf636c3bcfceb93a4ba7
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-02-13 10:11:03 +0000

    Web services generalized.

--- web/JSF/CompServer/src/main/java/org/bosik/diacomp/frontend/services/WebService.java
+++ web/JSF/CompServer/src/main/java/org/bosik/diacomp/frontend/services/WebService.java
@@ -1,6 +1,12 @@
 package org.bosik.diacomp.frontend.services;
 
+import org.bosik.diacomp.services.exceptions.CommonServiceException;
+import org.bosik.diacomp.services.exceptions.DeprecatedAPIException;
+import org.bosik.diacomp.services.exceptions.NotAuthorizedException;
+import org.bosik.diacomp.services.exceptions.UnsupportedAPIException;
 import org.bosik.diacomp.utils.MiscUtils;
+import org.bosik.diacomp.utils.ResponseBuilder;
+import org.bosik.diacomp.utils.StdResponse;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.client.apache.ApacheHttpClient;
@@ -33,5 +39,22 @@ public class WebService
 	public static Client getClient()
 	{
 		return client;
+	}
+
+	protected static void checkResponse(StdResponse resp) throws CommonServiceException
+	{
+		switch (resp.getCode())
+		{
+			case ResponseBuilder.CODE_OK:
+				return;
+			case ResponseBuilder.CODE_UNAUTHORIZED:
+				throw new NotAuthorizedException(resp.getResponse());
+			case ResponseBuilder.CODE_UNSUPPORTED_API:
+				throw new UnsupportedAPIException(resp.getResponse());
+			case ResponseBuilder.CODE_DEPRECATED_API:
+				throw new DeprecatedAPIException(resp.getResponse());
+			default: // case ResponseBuilder.CODE_FAIL:
+				throw new CommonServiceException(resp.getResponse());
+		}
 	}
 }
