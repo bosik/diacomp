a10ee932112d445ff56622fa25afda9ec6e271dd
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-02-14 13:27:43 +0000

    AuthDAO renamed to FakeAuthDAO.

--- trunk/web/JSF/CompServer/src/main/java/org/bosik/diacomp/backend/resources/FoodBaseResource.java
+++ trunk/web/JSF/CompServer/src/main/java/org/bosik/diacomp/backend/resources/FoodBaseResource.java
@@ -9,7 +9,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import org.bosik.diacomp.backend.dao.auth.AuthDAO;
+import org.bosik.diacomp.backend.dao.auth.FakeAuthDAO;
 import org.bosik.diacomp.services.exceptions.CommonServiceException;
 import org.bosik.diacomp.utils.ResponseBuilder;
 
@@ -21,7 +21,7 @@ public class FoodBaseResource
 
 	//private IDiaryDAO									diaryService				= new FakeDiaryDAO();
 
-	private AuthDAO										authService					= new AuthDAO();
+	private FakeAuthDAO										authService					= new FakeAuthDAO();
 
 	//	private static final Parser<String>					parserString				= new Parser<String>()
 	//																					{
