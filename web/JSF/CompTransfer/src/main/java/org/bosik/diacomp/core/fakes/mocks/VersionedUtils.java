0c99bb07fd075224aa2f5fd834737a7245bd7414
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-03-01 23:06:41 +0000

    Versioned GUID enumerator util introduced.

---
+++ web/JSF/CompTransfer/src/main/java/org/bosik/diacomp/core/fakes/mocks/VersionedUtils.java
@@ -0,0 +1,16 @@
+package org.bosik.diacomp.core.fakes.mocks;
+
+import java.util.List;
+import org.bosik.diacomp.core.entities.tech.Versioned;
+
+public class VersionedUtils
+{
+	public static <T> void enumerate(List<Versioned<T>> items)
+	{
+		int n = 1;
+		for (Versioned<T> item : items)
+		{
+			item.setId(String.valueOf(n++));
+		}
+	}
+}
