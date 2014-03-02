e02d7ad0d64d0212d607e4088363a4b4e2ed91af
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-03-02 00:51:19 +0000

    Duplicated test routines removed from android test project.

--- trunk/android/CompTest/src/org/bosik/diacomp/android/utils/TestUtils.java
+++ trunk/android/CompTest/src/org/bosik/diacomp/android/utils/TestUtils.java
@@ -1,52 +1,12 @@
 package org.bosik.diacomp.android.utils;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
 import junit.framework.TestCase;
-import org.bosik.diacomp.android.utils.AndroidUtils;
 
 @SuppressWarnings("static-method")
 public class TestUtils extends TestCase
 {
 	// private static final String TAG = TestUtils.class.getSimpleName();
-	public static final double	EPS			= 0.00001;
-	public static final long	EPS_TIME	= 5000;	// ms
-
-	public static Date date(int year, int month, int day)
-	{
-		Calendar c = Calendar.getInstance();
-		c.clear();
-		c.set(year, month - 1, day);
-		return c.getTime();
-	}
-
-	public static Date time(int year, int month, int day, int hour, int min, int sec)
-	{
-		Calendar c = Calendar.getInstance();
-		c.clear();
-		c.setTimeZone(TimeZone.getTimeZone("UTC"));
-		c.set(year, month - 1, day, hour, min, sec);
-		return c.getTime();
-	}
-
-	// private static String formatArray(byte array[])
-	// {
-	// StringBuilder sb = new StringBuilder("{");
-	//
-	// for (int i = 0; i < array.length; i++)
-	// {
-	// sb.append(array[i]);
-	// if (i < (array.length - 1))
-	// {
-	// sb.append(", ");
-	// }
-	// }
-	//
-	// sb.append("}");
-	// return sb.toString();
-	// }
 
 	public void testEncodingUtf8Cp1251Utf8Persistance()
 	{
