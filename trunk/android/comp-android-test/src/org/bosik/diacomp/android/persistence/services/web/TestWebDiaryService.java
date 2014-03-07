9ebe72e62fe5b07aaa5acf5b150446964bf8e68d
bosikgn <bosikgn@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-03-07 13:19:41 +0000

    Feature-based organizing: moving DiaryService interface & test together.

--- trunk/android/comp-android-test/src/org/bosik/diacomp/android/persistence/services/web/TestWebDiaryService.java
+++ trunk/android/comp-android-test/src/org/bosik/diacomp/android/persistence/services/web/TestWebDiaryService.java
@@ -2,8 +2,8 @@ package org.bosik.diacomp.android.persistence.services.web;
 
 import org.bosik.diacomp.android.backend.features.diary.WebDiaryService;
 import org.bosik.diacomp.android.persistence.services.web.utils.client.TestWebClient;
-import org.bosik.diacomp.core.persistence.services.TestDiaryService;
-import org.bosik.diacomp.core.services.DiaryService;
+import org.bosik.diacomp.core.services.diary.DiaryService;
+import org.bosik.diacomp.core.services.diary.TestDiaryService;
 
 public class TestWebDiaryService extends TestDiaryService
 {
--- trunk/android/comp-android/src/org/bosik/diacomp/android/backend/features/search/RelevantIndexator.java
+++ trunk/android/comp-android/src/org/bosik/diacomp/android/backend/features/search/RelevantIndexator.java
@@ -8,9 +8,9 @@ import org.bosik.diacomp.core.entities.business.diary.records.MealRecord;
 import org.bosik.diacomp.core.entities.business.interfaces.Tagged;
 import org.bosik.diacomp.core.entities.tech.Versioned;
 import org.bosik.diacomp.core.services.BaseService;
-import org.bosik.diacomp.core.services.DiaryService;
 import org.bosik.diacomp.core.services.DishBaseService;
 import org.bosik.diacomp.core.services.FoodBaseService;
+import org.bosik.diacomp.core.services.diary.DiaryService;
 import org.bosik.diacomp.core.utils.Utils;
 import android.util.Log;
 
