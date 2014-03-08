d5c4af08f37ced5d67ab0b310c18868a896a544f
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-03-08 00:46:56 +0000

    [tested] DiaryService tests generalized.

--- android/comp-android-test/src/org/bosik/diacomp/android/persistence/services/local/TestLocalDiaryService.java
+++ android/comp-android-test/src/org/bosik/diacomp/android/persistence/services/local/TestLocalDiaryService.java
@@ -1,15 +1,99 @@
 package org.bosik.diacomp.android.persistence.services.local;
 
+import org.bosik.diacomp.android.backend.features.diary.LocalDiaryService;
+import org.bosik.diacomp.core.services.diary.DiaryService;
+import org.bosik.diacomp.core.services.diary.TestDiaryService;
+import android.content.ContentResolver;
 import android.test.AndroidTestCase;
 
-public class TestLocalDiaryService extends AndroidTestCase// TestDiaryService
+public class TestLocalDiaryService extends AndroidTestCase
 {
-	// FIXME
+	private static TestDiaryService	test;
+
+	@Override
+	protected void setUp()
+	{
+		assertNotNull(getContext());
+		ContentResolver resolver = getContext().getContentResolver();
+		final DiaryService service = new LocalDiaryService(resolver);
+		test = new TestDiaryService()
+		{
+			@Override
+			protected DiaryService getService()
+			{
+				return service;
+			}
+		};
+		test.setUp();
+	}
+
 	// @Override
-	// protected DiaryService getDAO()
+	// public TestResult run()
 	// {
 	// assertNotNull(getContext());
 	// ContentResolver resolver = getContext().getContentResolver();
-	// return new LocalDiaryService(resolver);
+	// final DiaryService service = new LocalDiaryService(resolver);
+	//
+	// TestDiaryService test = new TestDiaryService()
+	// {
+	// @Override
+	// protected DiaryService getService()
+	// {
+	// return service;
 	// }
+	// };
+	// return test.run();
+	// }
+
+	// private static final String TAG = TestLocalDiaryService.class.getSimpleName();
+
+	// public void testAll() throws Throwable
+	// {
+	// assertNotNull(getContext());
+	// ContentResolver resolver = getContext().getContentResolver();
+	// final DiaryService service = new LocalDiaryService(resolver);
+	//
+	// TestDiaryService test = new TestDiaryService()
+	// {
+	// @Override
+	// protected DiaryService getService()
+	// {
+	// return service;
+	// }
+	// };
+	//
+	// test.setName("test_postRecordsGetRecordsViaPeriodAndGuid_Normal_RestoredExactly");
+	// Log.e(TAG, "countTestCases: " + String.valueOf(test.countTestCases()));
+	// Log.e(TAG, "testCount: " + String.valueOf(test.testCount()));
+	// }
+
+	public void test_getRecords_Deleting_Removed()
+	{
+		test.test_getRecords_Deleting_Removed();
+	}
+
+	public void test_getRecordsViaPeriod_Normal_RestoredOrdered()
+	{
+		test.test_getRecordsViaPeriod_Normal_RestoredOrdered();
+	}
+
+	public void test_getRecordsViaTimestamp_Normal_ok()
+	{
+		test.test_getRecordsViaTimestamp_Normal_ok();
+	}
+
+	public void test_PersistanceMultiple()
+	{
+		test.test_PersistanceMultiple();
+	}
+
+	public void test_postRecords_Update_UpdatedOk()
+	{
+		test.test_postRecords_Update_UpdatedOk();
+	}
+
+	public void test_postRecordsGetRecordsViaPeriodAndGuid_Normal_RestoredExactly()
+	{
+		test.test_postRecordsGetRecordsViaPeriodAndGuid_Normal_RestoredExactly();
+	}
 }
