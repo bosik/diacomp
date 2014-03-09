2147c4da9f1d23447d7bd5b7edf94fcaa841a07d
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-03-09 18:43:37 +0000

    WebClient test added.

--- trunk/android/comp-android-test/src/org/bosik/diacomp/android/persistence/services/web/utils/client/TestWebClient.java
+++ trunk/android/comp-android-test/src/org/bosik/diacomp/android/persistence/services/web/utils/client/TestWebClient.java
@@ -3,24 +3,27 @@ package org.bosik.diacomp.android.persistence.services.web.utils.client;
 import junit.framework.TestCase;
 import org.bosik.diacomp.android.backend.common.webclient.WebClient;
 import org.bosik.diacomp.android.backend.common.webclient.exceptions.WebClientException;
-import android.util.Log;
 
 public class TestWebClient extends TestCase
 {
-	private static final String	TAG	= TestWebClient.class.getSimpleName();
+	private static final String	TAG			= TestWebClient.class.getSimpleName();
+	// private static final String SERVER = "http://diacomp.16mb.com/";
+	private static final String	SERVER		= "http://192.168.0.104:8090/comp-server/";
+	private static final String	USERNAME	= "bosik-007@narod.ru";
+	private static final String	PASSWORD	= "devel0pment";
+	private static final int	TIMEOUT		= 3000;
 
 	private static WebClient	webClient;
 
+	/**
+	 * Constructs logged-in WebClient
+	 * 
+	 * @return
+	 */
 	public static WebClient getWebClient()
 	{
 		if (webClient == null)
 		{
-			// final String SERVER = "http://diacomp.16mb.com/";
-			final String SERVER = "http://192.168.0.104/";
-			final String USERNAME = "bosik-007@narod.ru";
-			final String PASSWORD = "devel0pment";
-			final int TIMEOUT = 3000;
-
 			webClient = new WebClient(TIMEOUT);
 			try
 			{
@@ -31,41 +34,16 @@ public class TestWebClient extends TestCase
 			}
 			catch (WebClientException e)
 			{
-				Log.e(TAG, "Failed to initialize WebClient: " + e.getLocalizedMessage() + "\n" + "Server: " + SERVER
-						+ "\n" + "Username: " + USERNAME + "\n" + "Password: " + PASSWORD);
-
+				throw new RuntimeException("Failed to initialize WebClient: " + e.getLocalizedMessage() + "\n"
+						+ "Server: " + SERVER + "\nUsername: " + USERNAME + "\nPassword: " + PASSWORD);
 			}
 		}
 		return webClient;
 	}
 
-	// TODO: implement
-
-	// @Override
-	// protected void setUp() throws Exception
-	// {
-	// super.setUp();
-	//
-	// webClient = new WebClient(TIMEOUT);
-	// webClient.setUsername(USERNAME);
-	// webClient.setPassword(PASSWORD);
-	// webClient.setServer(SERVER);
-	//
-	// webClient.login();
-	// }
-
-	// public void testLocalToServer()
-	// {
-	// Date localTime = Calendar.getInstance().getTime();
-	// Date serverTime = webClient.localToServer(localTime);
-	// Date localTimeAgain = webClient.serverToLocal(serverTime);
-	//
-	// assertEquals(localTime.getTime(), localTimeAgain.getTime());
-	// }
-
-	// public void testIsOnlineBoolean()
-	// {
-	// assertTrue(webClient.isOnline());
-	// assertTrue(webClient.isOnline(true));
-	// }
+	public void testLogin()
+	{
+		WebClient client = getWebClient();
+		assertEquals(true, client.isOnline());
+	}
 }
