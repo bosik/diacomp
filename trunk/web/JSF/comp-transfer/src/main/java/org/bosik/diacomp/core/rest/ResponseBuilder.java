5f5fc48c5b1f695ac68649b7214909ef1bbc80e0
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-03-09 13:15:38 +0000

    Statuses' comments added.

--- trunk/web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/rest/ResponseBuilder.java
+++ trunk/web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/rest/ResponseBuilder.java
@@ -2,12 +2,33 @@ package org.bosik.diacomp.core.rest;
 
 public class ResponseBuilder
 {
+	/**
+	 * "Requested operation done OK"
+	 */
 	public static final int	CODE_OK					= 0;
+	/**
+	 * "The supplied username/password pair is invalid"
+	 */
 	public static final int	CODE_BADCREDENTIALS		= 401;	// TODO
+	/**
+	 * "You need to be authorized to invoke this method"
+	 */
 	public static final int	CODE_UNAUTHORIZED		= 401;	// TODO
+	/**
+	 * "The requested method is not found"
+	 */
 	public static final int	CODE_NOTFOUND			= 404;
+	/**
+	 * "This API version is deprecated, but still supported. Consider updating your client as soon as possible."
+	 */
 	public static final int	CODE_DEPRECATED_API		= 4050;
+	/**
+	 * "This API version is not supported anymore. Can't do anything."
+	 */
 	public static final int	CODE_UNSUPPORTED_API	= 4051;
+	/**
+	 * "Requested operation failed"
+	 */
 	public static final int	CODE_FAIL				= 500;
 
 	public static String build(int code, String msg)
