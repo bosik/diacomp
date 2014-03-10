e7eab52ea9c72b0cedee2b7b3d843e76f1221c41
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-03-10 14:31:54 +0000

    WebDiaryService implementation.

--- trunk/android/comp-android/src/org/bosik/diacomp/android/backend/features/diary/WebDiaryService.java
+++ trunk/android/comp-android/src/org/bosik/diacomp/android/backend/features/diary/WebDiaryService.java
@@ -1,7 +1,11 @@
 package org.bosik.diacomp.android.backend.features.diary;
 
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.bosik.diacomp.android.backend.common.webclient.WebClient;
 import org.bosik.diacomp.core.entities.business.diary.DiaryRecord;
 import org.bosik.diacomp.core.entities.tech.Versioned;
@@ -10,8 +14,10 @@ import org.bosik.diacomp.core.persistence.serializers.ParserDiaryRecord;
 import org.bosik.diacomp.core.persistence.serializers.Serializer;
 import org.bosik.diacomp.core.persistence.serializers.utils.ParserVersioned;
 import org.bosik.diacomp.core.persistence.serializers.utils.SerializerAdapter;
+import org.bosik.diacomp.core.rest.StdResponse;
 import org.bosik.diacomp.core.services.diary.DiaryService;
 import org.bosik.diacomp.core.services.exceptions.CommonServiceException;
+import org.bosik.diacomp.core.utils.Utils;
 
 public class WebDiaryService implements DiaryService
 {
@@ -39,8 +45,20 @@ public class WebDiaryService implements DiaryService
 	@Override
 	public List<Versioned<DiaryRecord>> getRecords(List<String> guids) throws CommonServiceException
 	{
-		// TODO Auto-generated method stub
-		return null;
+		try
+		{
+			String sGuids = Utils.formatJSONArray(guids);
+			String query = "api/diary/guid/?guids=" + URLEncoder.encode(sGuids, "UTF-8");
+			String s = webClient.doGetSmart(query, WebClient.CODEPAGE_UTF8);
+			StdResponse resp = new StdResponse(s);
+			WebClient.checkResponse(resp);
+
+			return serializerV.readAll(resp.getResponse());
+		}
+		catch (Exception e)
+		{
+			throw new CommonServiceException(e);
+		}
 	}
 
 	@Override
@@ -61,7 +79,22 @@ public class WebDiaryService implements DiaryService
 	@Override
 	public void postRecords(List<Versioned<DiaryRecord>> records) throws CommonServiceException
 	{
-		// TODO Auto-generated method stub
+		try
+		{
+			String query = "api/diary/";
+			String items = serializerV.writeAll(records);
+
+			List<NameValuePair> params = new ArrayList<NameValuePair>();
+			params.add(new BasicNameValuePair("items", items));
+
+			String s = webClient.doPutSmart(query, params, WebClient.CODEPAGE_UTF8);
+			StdResponse resp = new StdResponse(s);
+			WebClient.checkResponse(resp);
+		}
+		catch (Exception e)
+		{
+			throw new CommonServiceException(e);
+		}
 	}
 
 	/* ======================= ROUTINES ========================= */
