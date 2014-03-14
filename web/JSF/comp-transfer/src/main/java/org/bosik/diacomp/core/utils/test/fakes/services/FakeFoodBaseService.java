2024b891db6de04235887b07bcdac099398f2692
bosikgn <bosikgn@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-03-14 13:29:08 +0000

    Common base API refactoring.

--- web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/utils/test/fakes/services/FakeFoodBaseService.java
+++ web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/utils/test/fakes/services/FakeFoodBaseService.java
@@ -1,75 +1,7 @@
 package org.bosik.diacomp.core.utils.test.fakes.services;
 
-import java.util.List;
-import org.bosik.diacomp.core.entities.business.foodbase.FoodItem;
-import org.bosik.diacomp.core.entities.tech.Versioned;
-import org.bosik.diacomp.core.services.FoodBaseService;
-import org.bosik.diacomp.core.services.exceptions.AlreadyDeletedException;
-import org.bosik.diacomp.core.services.exceptions.ItemNotFoundException;
-import org.bosik.diacomp.core.services.exceptions.PersistenceException;
 
-public class FakeFoodBaseService implements FoodBaseService
+public class FakeFoodBaseService// implements FoodBaseService
 {
-	@Override
-	public String add(Versioned<FoodItem> item) throws PersistenceException
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void delete(String id) throws ItemNotFoundException, AlreadyDeletedException
-	{
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public List<Versioned<FoodItem>> findAll()
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Versioned<FoodItem>> findAny(String filter)
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Versioned<FoodItem> findOne(String exactName)
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Versioned<FoodItem> findById(String guid)
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Versioned<FoodItem>> findSysAll()
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Versioned<FoodItem> findSysById(String id)
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void update(Versioned<FoodItem> item) throws ItemNotFoundException, PersistenceException
-	{
-		// TODO Auto-generated method stub
-
-	}
+	// TODO: implement
 }
