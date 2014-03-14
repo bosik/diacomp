0e7f87a9f3e83344bea355f72d1db10c4ad881db
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-03-14 23:09:43 +0000

    Frontend tests are separated.

---
+++ trunk/web/JSF/comp-server/src/test/java/org/bosik/diacomp/web/frontend/features/diary/TestDiaryWebService.java
@@ -0,0 +1,21 @@
+package org.bosik.diacomp.web.frontend.features.diary;
+
+import org.bosik.diacomp.core.services.diary.DiaryService;
+import org.bosik.diacomp.core.services.diary.TestDiaryServiceCommon;
+import org.bosik.diacomp.core.services.diary.TestDiaryService;
+import org.bosik.diacomp.web.backend.common.Config;
+import org.bosik.diacomp.web.frontend.features.auth.AuthRestClient;
+import org.bosik.diacomp.web.frontend.features.diary.DiaryRestClient;
+
+public class TestDiaryWebService extends TestDiaryServiceCommon implements TestDiaryService
+{
+	@Override
+	protected DiaryService getService()
+	{
+		String login = Config.getLogin();
+		String pass = Config.getPassword();
+		int apiVersion = Config.getAPICurrent();
+
+		return new DiaryRestClient(new AuthRestClient(), login, pass, apiVersion);
+	}
+}
