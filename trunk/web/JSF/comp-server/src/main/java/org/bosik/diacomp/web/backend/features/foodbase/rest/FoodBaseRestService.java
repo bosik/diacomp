812ad24d450b1c61e9e0d5ddddbf86ad53b27d08
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-03-15 11:36:00 +0000

    Foodbase REST @PUT fix.

--- trunk/web/JSF/comp-server/src/main/java/org/bosik/diacomp/web/backend/features/foodbase/rest/FoodBaseRestService.java
+++ trunk/web/JSF/comp-server/src/main/java/org/bosik/diacomp/web/backend/features/foodbase/rest/FoodBaseRestService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -101,7 +102,8 @@ public class FoodBaseRestService
 		}
 	}
 
-	@Produces(MediaType.APPLICATION_JSON)
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 	public Response postRecords(@FormParam("items") String parItems) throws CommonServiceException
 	{
 		try
