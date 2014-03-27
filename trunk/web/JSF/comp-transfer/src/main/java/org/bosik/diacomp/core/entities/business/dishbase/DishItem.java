57b15a040c9ee740bfd4c7bbd556e0e2445ce284
bosikgn <bosikgn@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-03-27 15:24:35 +0000

    DishItem implementation.

--- trunk/web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/entities/business/dishbase/DishItem.java
+++ trunk/web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/entities/business/dishbase/DishItem.java
@@ -1,52 +1,143 @@
 package org.bosik.diacomp.core.entities.business.dishbase;
 
 import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+import org.bosik.diacomp.core.entities.business.FoodMassed;
 import org.bosik.diacomp.core.entities.business.interfaces.NamedRelativeTagged;
+import com.google.gson.annotations.SerializedName;
 
 public class DishItem implements NamedRelativeTagged, Serializable
 {
-	private static final long	serialVersionUID	= 1L;
+	private static final long		serialVersionUID	= 1L;
+
+	@SerializedName("name")
+	private String					name;
+
+	@SerializedName("tag")
+	private int						tag;
+
+	@SerializedName("mass")
+	private Double					mass;
+
+	private final List<FoodMassed>	content				= new LinkedList<FoodMassed>();
 
 	// ================================ GET / SET ================================
 
+	private double getRealMass()
+	{
+		if (mass != null)
+		{
+			return mass;
+		}
+
+		double result = 0.0;
+		for (FoodMassed item : content)
+		{
+			result += item.getMass();
+		}
+		return result;
+	}
+
+	@Override
 	public String getName()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	@Override
 	public int getTag()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		return tag;
 	}
 
+	@Override
 	public void setTag(int tag)
 	{
-		// TODO Auto-generated method stub
+		this.tag = tag;
 	}
 
+	@Override
 	public double getRelProts()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		double result = 0.0;
+		for (FoodMassed item : content)
+		{
+			result += item.getProts();
+		}
+
+		// TODO: implement zero-check
+		return result / getRealMass();
 	}
 
+	@Override
 	public double getRelFats()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		double result = 0.0;
+		for (FoodMassed item : content)
+		{
+			result += item.getFats();
+		}
+
+		// TODO: implement zero-check
+		return result / getRealMass();
 	}
 
+	@Override
 	public double getRelCarbs()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		double result = 0.0;
+		for (FoodMassed item : content)
+		{
+			result += item.getCarbs();
+		}
+
+		// TODO: implement zero-check
+		return result / getRealMass();
 	}
 
+	@Override
 	public double getRelValue()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		double result = 0.0;
+		for (FoodMassed item : content)
+		{
+			result += item.getValue();
+		}
+
+		// TODO: implement zero-check
+		return result / getRealMass();
+	}
+
+	// =================================== LIST METHODS ===================================
+
+	public void add(FoodMassed item)
+	{
+		if (item != null)
+		{
+			content.add(item);
+		}
+
+		throw new NullPointerException("Dish item can't be null");
+	}
+
+	public void remove(int index)
+	{
+		content.remove(index);
+	}
+
+	public int count()
+	{
+		return content.size();
+	}
+
+	public FoodMassed get(int index)
+	{
+		return content.get(index);
 	}
 }
