31a7cbb23c771713b3a8a834b3f06a651ace71e3
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-03-29 22:36:01 +0000

    TestSerializerDiaryRecord changed to test out-of-box versioned serializer.

---
+++ trunk/web/JSF/comp-transfer/src/test/java/org/bosik/diacomp/core/persistence/serializers/diary/TestSerializerDiaryRecord.java
@@ -0,0 +1,31 @@
+package org.bosik.diacomp.core.persistence.serializers.diary;
+
+import org.bosik.diacomp.core.entities.business.diary.DiaryRecord;
+import org.bosik.diacomp.core.entities.tech.Versioned;
+import org.bosik.diacomp.core.persistence.serializers.Serializer;
+import org.bosik.diacomp.core.persistence.serializers.TestSerializer;
+import org.bosik.diacomp.core.persistence.serializers.ready.SerializerDiaryRecord;
+import org.bosik.diacomp.core.utils.test.fakes.mocks.Mock;
+import org.bosik.diacomp.core.utils.test.fakes.mocks.MockDiaryRecord;
+import org.bosik.diacomp.core.utils.test.fakes.mocks.MockVersionedConverter;
+
+public class TestSerializerDiaryRecord extends TestSerializer<Versioned<DiaryRecord>>
+{
+	private static final Mock<DiaryRecord>					mockFoodItem			= new MockDiaryRecord();
+	private static final Mock<Versioned<DiaryRecord>>		mockFoodItemVersioned	= new MockVersionedConverter<DiaryRecord>(
+																							mockFoodItem);
+
+	private static final Serializer<Versioned<DiaryRecord>>	serializerAdapter		= new SerializerDiaryRecord();
+
+	@Override
+	protected Mock<Versioned<DiaryRecord>> getMock()
+	{
+		return mockFoodItemVersioned;
+	}
+
+	@Override
+	protected Serializer<Versioned<DiaryRecord>> getSerializer()
+	{
+		return serializerAdapter;
+	}
+}
