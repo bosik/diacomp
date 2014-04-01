a7fccc4754a5053423dacb29aac1a1777c80e64e
bosikgn <bosikgn@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-04-01 15:08:01 +0000

    DiarySyncService generalized.

--- web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/services/diary/DiaryService.java
+++ web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/services/diary/DiaryService.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 import org.bosik.diacomp.core.entities.business.diary.DiaryRecord;
 import org.bosik.diacomp.core.entities.tech.Versioned;
+import org.bosik.diacomp.core.services.ObjectService;
 import org.bosik.diacomp.core.services.exceptions.CommonServiceException;
 
 /**
@@ -11,26 +12,8 @@ import org.bosik.diacomp.core.services.exceptions.CommonServiceException;
  *
  * @author Bosik
  */
-public interface DiaryService
+public interface DiaryService extends ObjectService<DiaryRecord>
 {
-	/**
-	 * Returns records with the specified GUID
-	 *
-	 * @param guid
-	 * @return Item if found, null otherwise
-	 * @throws CommonServiceException
-	 */
-	public Versioned<DiaryRecord> getRecord(String guid) throws CommonServiceException;
-
-	/**
-	 * Returns list of records which were modified after the specified time (both removed or not)
-	 *
-	 * @param time
-	 * @return
-	 * @throws CommonServiceException
-	 */
-	public List<Versioned<DiaryRecord>> getRecords(Date time) throws CommonServiceException;
-
 	/**
 	 * Returns list of non-deleted records for the specified time interval
 	 *
@@ -42,12 +25,4 @@ public interface DiaryService
 	 */
 	public List<Versioned<DiaryRecord>> getRecords(Date fromTime, Date toTime, boolean includeRemoved)
 			throws CommonServiceException;
-
-	/**
-	 * Persists records (create if not exist, update otherwise)
-	 *
-	 * @param records
-	 * @throws CommonServiceException
-	 */
-	public void postRecords(List<Versioned<DiaryRecord>> records) throws CommonServiceException;
 }
