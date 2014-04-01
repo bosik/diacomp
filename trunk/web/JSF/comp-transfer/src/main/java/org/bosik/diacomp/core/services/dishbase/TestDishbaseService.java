68a05475c0eb6cf9ff63b4be9df2ee5abfa20c65
bosikgn <bosikgn@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-04-01 11:19:43 +0000

    TestDishbaseService added.

---
+++ trunk/web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/services/dishbase/TestDishbaseService.java
@@ -0,0 +1,14 @@
+package org.bosik.diacomp.core.services.dishbase;
+
+public interface TestDishbaseService
+{
+	void test_addFindById_single_PersistedOk();
+
+	void test_addFindAll_single_ReturnedNonEmpty();
+
+	void test_addFindChanged_single_ReturnedNonEmpty();
+
+	void test_delete_notFound_exceptionRaised();
+
+	void test_addFindAny_single_ok();
+}
