bd5c6b9784d1242c46dc8062aec0daf34c586752
bosikgn <bosikgn@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-04-02 12:32:25 +0000

    Minor: FIXME comments cleared.

--- web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/test/fakes/services/FakeDiaryService.java
+++ web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/test/fakes/services/FakeDiaryService.java
@@ -1,7 +1,5 @@
 package org.bosik.diacomp.core.test.fakes.services;
 
-
-// FIXME: implement in-memory service
 public class FakeDiaryService //implements DiaryService
 {
 	// TODO: implement if needed
--- web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/test/fakes/services/FakeFoodBaseService.java
+++ web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/test/fakes/services/FakeFoodBaseService.java
@@ -1,6 +1,5 @@
 package org.bosik.diacomp.core.test.fakes.services;
 
-
 public class FakeFoodBaseService// implements FoodBaseService
 {
 	// TODO: implement
