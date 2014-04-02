e9181ed56a26c056cdc67ab6e5692077dfa488dc
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-04-02 20:52:51 +0000

    Dishbase tested. All tests succeed.

---
+++ trunk/android/comp-android-test/src/org/bosik/diacomp/android/backend/features/dishbase/TestDishBaseLocalService.java
@@ -0,0 +1,63 @@
+package org.bosik.diacomp.android.backend.features.dishbase;
+
+import org.bosik.diacomp.core.services.dishbase.DishBaseService;
+import org.bosik.diacomp.core.services.dishbase.TestDishbaseService;
+import org.bosik.diacomp.core.services.dishbase.TestDishbaseServiceCommon;
+import android.content.ContentResolver;
+import android.test.AndroidTestCase;
+
+public class TestDishBaseLocalService extends AndroidTestCase implements TestDishbaseService
+{
+	private TestDishbaseServiceCommon	tester;
+
+	@Override
+	public void setUp()
+	{
+		tester = new TestDishbaseServiceCommon()
+		{
+			@Override
+			protected DishBaseService getService()
+			{
+				return TestDishBaseLocalService.this.getService();
+			}
+		};
+		tester.init();
+	}
+
+	public DishBaseService getService()
+	{
+		assertNotNull("Context is null", getContext());
+		ContentResolver resolver = getContext().getContentResolver();
+		return new DishBaseLocalService(resolver);
+	}
+
+	@Override
+	public void test_addFindById_single_PersistedOk()
+	{
+		tester.test_addFindById_single_PersistedOk();
+	}
+
+	@Override
+	public void test_addFindAll_single_ReturnedNonEmpty()
+	{
+		tester.test_addFindChanged_single_ReturnedNonEmpty();
+	}
+
+	@Override
+	public void test_addFindChanged_single_ReturnedNonEmpty()
+	{
+		tester.test_addFindChanged_single_ReturnedNonEmpty();
+	}
+
+	@Override
+	public void test_delete_notFound_exceptionRaised()
+	{
+		tester.test_delete_notFound_exceptionRaised();
+	}
+
+	@Override
+	public void test_addFindAny_single_ok()
+	{
+		tester.test_addFindAny_single_ok();
+	}
+}
