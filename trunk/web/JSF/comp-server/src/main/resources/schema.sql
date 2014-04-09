0e9f5e9ad54119728a60807caf45e6314e220052
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-04-09 06:50:16 +0000

    DB schema added.

---
+++ trunk/web/JSF/comp-server/src/main/resources/schema.sql
@@ -0,0 +1,56 @@
+delimiter $$
+
+CREATE TABLE `diary2` (
+  `_GUID` char(32) CHARACTER SET armscii8 NOT NULL,
+  `_UserID` int(10) unsigned NOT NULL,
+  `_TimeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `_Version` int(11) NOT NULL,
+  `_Deleted` int(1) DEFAULT '0',
+  `_Content` text,
+  `_TimeCache` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`_GUID`)
+) ENGINE=MyISAM AUTO_INCREMENT=1526 DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `dishbase2` (
+  `_GUID` char(32) NOT NULL,
+  `_UserID` int(11) NOT NULL,
+  `_TimeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `_Version` int(11) NOT NULL,
+  `_Deleted` int(1) NOT NULL DEFAULT '0',
+  `_Content` text NOT NULL,
+  `_NameCache` varchar(100) NOT NULL,
+  PRIMARY KEY (`_GUID`),
+  UNIQUE KEY `_GUID_UNIQUE` (`_GUID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `foodbase2` (
+  `_GUID` char(32) NOT NULL,
+  `_UserID` int(11) NOT NULL,
+  `_TimeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `_Version` int(11) NOT NULL,
+  `_Deleted` int(1) NOT NULL DEFAULT '0',
+  `_Content` text NOT NULL,
+  `_NameCache` varchar(100) NOT NULL,
+  PRIMARY KEY (`_GUID`),
+  UNIQUE KEY `_GUID_UNIQUE` (`_GUID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
+
+delimiter $$
+
+CREATE TABLE `user` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Login` varchar(40) CHARACTER SET cp1251 NOT NULL,
+  `HashPass` char(32) CHARACTER SET cp1251 COLLATE cp1251_general_cs NOT NULL,
+  `DateReg` datetime DEFAULT NULL,
+  `DateLogin` datetime DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci$$
+
+
