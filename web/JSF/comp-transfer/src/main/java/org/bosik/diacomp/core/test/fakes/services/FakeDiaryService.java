3d982b1bd4c558bb14030eefcd9fe588f02eb162
bosikgn <bosikgn@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-04-10 09:56:32 +0000

    Fake diary service prepared.

--- web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/test/fakes/services/FakeDiaryService.java
+++ web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/test/fakes/services/FakeDiaryService.java
@@ -1,6 +1,49 @@
 package org.bosik.diacomp.core.test.fakes.services;
 
-public class FakeDiaryService //implements DiaryService
+import java.util.Date;
+import java.util.List;
+import org.bosik.diacomp.core.entities.business.diary.DiaryRecord;
+import org.bosik.diacomp.core.entities.tech.Versioned;
+import org.bosik.diacomp.core.services.diary.DiaryService;
+import org.bosik.diacomp.core.services.exceptions.AlreadyDeletedException;
+import org.bosik.diacomp.core.services.exceptions.CommonServiceException;
+import org.bosik.diacomp.core.services.exceptions.NotFoundException;
+
+public class FakeDiaryService implements DiaryService
 {
-	// TODO: implement if needed
+	// TODO: implement
+
+	@Override
+	public void delete(String id) throws NotFoundException, AlreadyDeletedException
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public Versioned<DiaryRecord> findById(String id) throws CommonServiceException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Versioned<DiaryRecord>> findChanged(Date since) throws CommonServiceException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(List<Versioned<DiaryRecord>> items) throws CommonServiceException
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public List<Versioned<DiaryRecord>> findBetween(Date fromTime, Date toTime, boolean includeRemoved)
+			throws CommonServiceException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
