d20a1ee599b154c5d478c6a5c95bd71b023ed5af
bosikgn <bosikgn@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-04-10 09:59:27 +0000

    AnalyzeService moved to comp-transfer.

---
+++ trunk/web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/services/analyze/entities/TimePoint.java
@@ -0,0 +1,27 @@
+package org.bosik.diacomp.core.services.analyze.entities;
+
+public class TimePoint
+{
+	private int		time;
+	private double	value;
+
+	public int getTime()
+	{
+		return time;
+	}
+
+	public void setTime(int time)
+	{
+		this.time = time;
+	}
+
+	public double getValue()
+	{
+		return value;
+	}
+
+	public void setValue(double value)
+	{
+		this.value = value;
+	}
+}
---
+++ trunk/web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/services/analyze/entities/WeightedTimePoint.java
@@ -0,0 +1,16 @@
+package org.bosik.diacomp.core.services.analyze.entities;
+
+public class WeightedTimePoint extends TimePoint
+{
+	private double	weight;
+
+	public double getWeight()
+	{
+		return weight;
+	}
+
+	public void setWeight(double weight)
+	{
+		this.weight = weight;
+	}
+}
