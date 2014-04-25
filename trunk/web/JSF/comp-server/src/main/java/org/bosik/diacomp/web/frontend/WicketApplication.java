67b6ce95f029a053cad30e8389e2160bb63524a2
bosikgn <bosikgn@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-04-25 19:09:12 +0000

    AboutPage added.

--- trunk/web/JSF/comp-server/src/main/java/org/bosik/diacomp/web/frontend/WicketApplication.java
+++ trunk/web/JSF/comp-server/src/main/java/org/bosik/diacomp/web/frontend/WicketApplication.java
@@ -2,6 +2,7 @@ package org.bosik.diacomp.web.frontend;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.bosik.diacomp.web.frontend.wicket.pages.about.AboutPage;
 import org.bosik.diacomp.web.frontend.wicket.pages.diary.DiaryPage;
 
 public class WicketApplication extends WebApplication
@@ -18,5 +19,6 @@ public class WicketApplication extends WebApplication
 		super.init();
 
 		mountPage("/diary", DiaryPage.class);
+		mountPage("/about", AboutPage.class);
 	}
 }
