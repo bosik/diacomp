70d4ab184fc01164af03cce01d59fd70d84a9c6b
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-04-27 20:09:16 +0000

    Minor: organized imports.

--- trunk/android/comp-android-test/src/org/bosik/diacomp/android/backend/common/webclient/TestWebClient.java
+++ trunk/android/comp-android-test/src/org/bosik/diacomp/android/backend/common/webclient/TestWebClient.java
@@ -1,14 +1,13 @@
 package org.bosik.diacomp.android.backend.common.webclient;
 
 import junit.framework.TestCase;
-import org.bosik.diacomp.android.backend.common.webclient.WebClient;
 import org.bosik.diacomp.android.backend.common.webclient.exceptions.WebClientException;
 
 public class TestWebClient extends TestCase
 {
 	// private static final String TAG = TestWebClient.class.getSimpleName();
 	// private static final String SERVER = "http://diacomp.16mb.com/";
-	private static final String	SERVER		= "http://192.168.0.104:8090/comp-server/";
+	private static final String	SERVER		= "http://192.168.0.103:8090/comp-server/";
 	private static final String	USERNAME	= "bosik-007@narod.ru";
 	private static final String	PASSWORD	= "devel0pment";
 	private static final int	TIMEOUT		= 3000;
