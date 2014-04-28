a3530b46aca1f609d662eb29fec44b3ab66e3778
bosikgn <bosikgn@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-04-28 13:08:25 +0000

    FoodBase page added. Download page added.

--- web/JSF/comp-server/src/main/java/org/bosik/diacomp/web/frontend/wicket/WicketApplication.java
+++ web/JSF/comp-server/src/main/java/org/bosik/diacomp/web/frontend/wicket/WicketApplication.java
@@ -4,6 +4,8 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.bosik.diacomp.web.frontend.wicket.pages.about.AboutPage;
 import org.bosik.diacomp.web.frontend.wicket.pages.diary.DiaryPage;
+import org.bosik.diacomp.web.frontend.wicket.pages.download.DownloadPage;
+import org.bosik.diacomp.web.frontend.wicket.pages.foodbase.FoodBasePage;
 
 public class WicketApplication extends WebApplication
 {
@@ -19,6 +21,8 @@ public class WicketApplication extends WebApplication
 		super.init();
 
 		mountPage("/diary", DiaryPage.class);
+		mountPage("/base", FoodBasePage.class);
 		mountPage("/about", AboutPage.class);
+		mountPage("/download", DownloadPage.class);
 	}
 }
