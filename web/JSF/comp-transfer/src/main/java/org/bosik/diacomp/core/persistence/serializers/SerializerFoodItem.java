9c7efd2796a11afe58d34ddbc2efffb0e12f78f1
bosikgn <bosikgn@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-05-06 14:49:51 +0000

    Serializers re-factored.

--- web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/persistence/serializers/SerializerFoodItem.java
+++ web/JSF/comp-transfer/src/main/java/org/bosik/diacomp/core/persistence/serializers/SerializerFoodItem.java
@@ -1,41 +1,15 @@
 package org.bosik.diacomp.core.persistence.serializers;
 
-import java.util.List;
 import org.bosik.diacomp.core.entities.business.foodbase.FoodItem;
 import org.bosik.diacomp.core.entities.tech.Versioned;
-import org.bosik.diacomp.core.persistence.parsers.Parser;
 import org.bosik.diacomp.core.persistence.parsers.ParserFoodItem;
 import org.bosik.diacomp.core.persistence.utils.ParserVersioned;
 import org.bosik.diacomp.core.persistence.utils.SerializerAdapter;
 
-public class SerializerFoodItem implements Serializer<Versioned<FoodItem>>
+public class SerializerFoodItem extends SerializerAdapter<Versioned<FoodItem>>
 {
-	private final Parser<FoodItem>					parser			= new ParserFoodItem();
-	private final Parser<Versioned<FoodItem>>		parserVersioned	= new ParserVersioned<FoodItem>(parser);
-	private final Serializer<Versioned<FoodItem>>	serializer		= new SerializerAdapter<Versioned<FoodItem>>(
-																		parserVersioned);
-
-	@Override
-	public Versioned<FoodItem> read(String s)
+	public SerializerFoodItem()
 	{
-		return serializer.read(s);
-	}
-
-	@Override
-	public List<Versioned<FoodItem>> readAll(String s)
-	{
-		return serializer.readAll(s);
-	}
-
-	@Override
-	public String write(Versioned<FoodItem> object)
-	{
-		return serializer.write(object);
-	}
-
-	@Override
-	public String writeAll(List<Versioned<FoodItem>> objects)
-	{
-		return serializer.writeAll(objects);
+		super(new ParserVersioned<FoodItem>(new ParserFoodItem()));
 	}
 }
