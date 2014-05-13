dfe2c1e2229f2eb50ae0840278d5901a255d628e
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-05-13 21:52:04 +0000

    Common requestMass() dialog implemented.

--- android/comp-android/src/org/bosik/diacomp/android/frontend/UIUtils.java
+++ android/comp-android/src/org/bosik/diacomp/android/frontend/UIUtils.java
@@ -1,6 +1,13 @@
 package org.bosik.diacomp.android.frontend;
 
+import org.bosik.diacomp.core.utils.Utils;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.text.InputType;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class UIUtils
@@ -8,5 +15,67 @@ public class UIUtils
 	public static void showTip(Activity activity, String Msg)
 	{
 		Toast.makeText(activity.getApplicationContext(), Msg, Toast.LENGTH_SHORT).show();
+	}
+
+	public interface OnSubmit
+	{
+		void onSubmit(Double mass);
+	}
+
+	public static void requestMass(final Context context, String title, String message, String defaultMass,
+			final OnSubmit e)
+	{
+		Builder builder = new AlertDialog.Builder(context);
+
+		final EditText input = new EditText(context);
+
+		input.setText(defaultMass);
+		input.setInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL);
+		// TODO: localize
+		builder.setTitle(title);
+		builder.setMessage(message);
+		builder.setView(input);
+		builder.setPositiveButton("Ok", new DialogInterface.OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface dialog, int whichButton)
+			{
+				String text = input.getText().toString();
+
+				try
+				{
+					if (text.isEmpty())
+					{
+						e.onSubmit(null);
+					}
+					else
+					{
+						double mass = Utils.parseExpression(text);
+						if (mass > Utils.EPS)
+						{
+							e.onSubmit(mass);
+						}
+						else
+						{
+							e.onSubmit(null);
+						}
+					}
+				}
+				catch (NumberFormatException e)
+				{
+					// TODO: localize
+					UIUtils.showTip((Activity) context, "Wrong mass");
+				}
+			}
+		});
+		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener()
+		{
+			@Override
+			public void onClick(DialogInterface dialog, int whichButton)
+			{
+				// Do nothing.
+			}
+		});
+		builder.show();
 	}
 }
