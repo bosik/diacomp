ed00aebeafc8a6d3fcd3d191b5181f27ddce4a86
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-06-04 01:06:46 +0000

    DiaryPage notifying removed.

--- win32/development/src/bo/DiaryRecords.pas
+++ win32/development/src/bo/DiaryRecords.pas
@@ -20,23 +20,14 @@ type
   TCustomRecord = class (TVersioned)
   private
     FTime: TDateTime;               // UTC
-
-    FSilentMode: boolean;           // transient
-    FSilentlyModified: boolean;     // transient
-    FOnChange: TEventRecordChanged; // transient
   protected
-    procedure NotifyPage; deprecated;
     function GetTime(): integer;
     procedure SetNativeTime(Value: TDateTime);
   public
-    procedure BeginUpdate;
-    constructor Create; overload;
-    procedure EndUpdate;
     function RecType: TClassCustomRecord;
 
     property Time: integer read GetTime;
     property NativeTime: TDateTime read FTime write SetNativeTime;
-    property OnChange: TEventRecordChanged read FOnChange write FOnChange; 
   end;
 
   TRecordList = array of TCustomRecord;
@@ -82,8 +73,6 @@ type
     function GetFood(Index: integer): TFoodMassed;
     function GetProp(Index: integer): real;
     procedure SetShortMeal(Value: boolean);
-
-    procedure MealChangeHandler(Sender: TObject);
   public
     function Add(Food: TFoodMassed): integer;
     constructor Create(ATime: TDateTime = 0; AShortMeal: boolean = False);
@@ -156,55 +145,10 @@ end;
 { TCustomRecord }
 
 {==============================================================================}
-procedure TCustomRecord.BeginUpdate;
-{==============================================================================}
-begin
-  FSilentMode := True;
-end;
-
-{==============================================================================}
-constructor TCustomRecord.Create;
-{==============================================================================}
-begin
-  inherited Create();
-  FOnChange := nil;
-  FSilentMode := False;
-  FSilentlyModified := False;
-end;
-
-{==============================================================================}
-procedure TCustomRecord.EndUpdate;
-{==============================================================================}
-begin
-  { Идея в том, чтобы множественные изменения записи оборачивать блоком }
-  { BeginUpdate...EndUpdate и оповещать страницу об изменениях только   }
-  { один раз при вызове EndUpdate. }
-  FSilentMode := False;
-  if FSilentlyModified then
-    NotifyPage;
-end;
-
-{==============================================================================}
 function TCustomRecord.GetTime: integer;
 {==============================================================================}
 begin
   Result := Round(UTCToLocal(FTime) * MinPerDay) mod MinPerDay;
-end;
-
-{==============================================================================}
-procedure TCustomRecord.NotifyPage;
-{==============================================================================}
-begin
-  begin
-    if (FSilentMode) then
-      FSilentlyModified := True
-    else
-    if (Assigned(FOnChange)) then
-    begin
-      FOnChange(Self);
-      FSilentlyModified := False;
-    end;
-  end;
 end;
 
 {==============================================================================}
@@ -218,13 +162,7 @@ end;
 procedure TCustomRecord.SetNativeTime(Value: TDateTime);
 {==============================================================================}
 begin
-  if (FTime <> Value) //and (CheckTime(Value))
-  then
-  begin
-    FTime := Value;
-    NotifyPage;
-  end;
-  // TODO: throw exception!
+  FTime := Value;
 end;
 
 { TBloodRecord }
@@ -262,7 +200,6 @@ begin
   if (FFinger <> NewFinger) and (CheckFinger(NewFinger)) then
   begin
     FFinger := NewFinger;
-    NotifyPage;
   end;
 end;
 
@@ -273,7 +210,6 @@ begin
   if (FValue <> NewValue) and (CheckValue(NewValue)) then
   begin
     FValue := NewValue;
-    NotifyPage;
   end;
 end;
 
@@ -302,7 +238,6 @@ begin
   if (FValue <> NewValue) and (CheckValue(NewValue)) then
   begin
     FValue := NewValue;
-    NotifyPage;
   end;
 end;
 
@@ -314,12 +249,9 @@ function TMealRecord.Add(Food: TFoodMassed): integer;
 begin
   if Food <> nil then
   begin
-    Food.OnChange := MealChangeHandler;
-
     Result := Length(FFood);
-    SetLength(FFood,Result+1);
+    SetLength(FFood, Result + 1);
     FFood[Result] := Food;
-    NotifyPage;
   end else
     Result := -1;
 end;
@@ -342,8 +274,7 @@ begin
   begin
     for i := 0 to High(FFood) do
       FFood[i].Free;
-    SetLength(FFood,0);
-    NotifyPage;
+    SetLength(FFood, 0);
   end;
 end;
 
@@ -360,7 +291,6 @@ end;
 destructor TMealRecord.Destroy;
 {==============================================================================}
 begin
-  FOnChange := nil; { чтобы при очистке родительская страница ничего не увидела }
   Clear;
 end;
 
@@ -378,8 +308,6 @@ begin
     Temp := FFood[Index1];
     FFood[Index1] := FFood[Index2];
     FFood[Index2] := Temp;
-
-    NotifyPage;
   end;
 end;
 
@@ -418,11 +346,7 @@ end;
 procedure TMealRecord.SetShortMeal(Value: boolean);
 {==============================================================================}
 begin
-  if FShortMeal <> Value then
-  begin
-    FShortMeal := Value;
-    NotifyPage;
-  end;
+  FShortMeal := Value;
 end;
 
 {==============================================================================}
@@ -436,15 +360,6 @@ begin
   for i := Index to High(FFood) - 1 do
     FFood[i] := FFood[i + 1];
   SetLength(FFood, Length(FFood) - 1);
-
-  NotifyPage;
-end;
-
-{==============================================================================}
-procedure TMealRecord.MealChangeHandler(Sender: TObject);
-{==============================================================================}
-begin
-  NotifyPage();
 end;
 
 { TNoteRecord }
@@ -464,7 +379,7 @@ procedure TNoteRecord.SetText(const Value: string);
 
   function Check(const S: string): string;
   const
-    Reserved = [#10,#13];
+    Reserved = [#10, #13];
   var
     i,j: integer;
   begin
@@ -479,15 +394,8 @@ procedure TNoteRecord.SetText(const Value: string);
     SetLength(Result, j);
   end;
 
-var
-  NewValue: string;
 begin
-  NewValue := Check(Value);
-  if NewValue <> FText then
-  begin
-    FText := NewValue;
-    NotifyPage;
-  end;
+  FText := Check(Value);
 end;
 
 {==============================================================================}
