71dc19c1678f09938d9d32ec31239a9b9cda3fe7
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-06-04 02:37:52 +0000

    DiaryDAO.Add() naive implementation moved to base class.

--- trunk/win32/development/src/backend/features/diary/DiaryDAO.pas
+++ trunk/win32/development/src/backend/features/diary/DiaryDAO.pas
@@ -4,16 +4,30 @@ interface
 
 uses
   DiaryRecords,
-  ObjectService;
+  ObjectService,
+  BusinessObjects;
 
 type
   TDiaryDAO = class (TObjectService)
   public
-    procedure Add(const R: TCustomRecord); virtual; abstract;
+    procedure Add(const R: TCustomRecord); virtual;
     function FindPeriod(TimeFrom, TimeTo: TDateTime): TRecordList; virtual; abstract;
   end;
 
 implementation
+
+{ TDiaryDAO }
+
+{==============================================================================}
+procedure TDiaryDAO.Add(const R: TCustomRecord);
+{==============================================================================}
+var
+  Recs: TVersionedList;
+begin
+  SetLength(Recs, 1);
+  Recs[0] := R;
+  Save(Recs);
+end;
 
 end.
 
