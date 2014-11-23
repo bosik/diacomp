ecd497e88ea91bfa5faf06d2fdae452ab5b09cc9
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-11-23 16:26:07 +0000

    Diary icons introduced.

--- web/JSF/comp-server/src/main/java/org/bosik/diacomp/web/frontend/wicket/components/diary/ins/DiaryPanelIns.java
+++ web/JSF/comp-server/src/main/java/org/bosik/diacomp/web/frontend/wicket/components/diary/ins/DiaryPanelIns.java
@@ -1,7 +1,10 @@
 package org.bosik.diacomp.web.frontend.wicket.components.diary.ins;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.Image;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
 import org.bosik.diacomp.core.entities.business.diary.records.InsRecord;
 import org.bosik.diacomp.core.utils.Utils;
 
@@ -12,6 +15,7 @@ public class DiaryPanelIns extends Panel
 	public DiaryPanelIns(String id, InsRecord rec)
 	{
 		super(id);
+		add(new Image("icon", new Model<String>("icon.png")).add(AttributeModifier.replace("title", "Инъекция")));
 		add(new Label("time", Utils.formatTimeLocalShort(rec.getTime())));
 		add(new Label("value", formatInsValue(rec.getValue())));
 	}
