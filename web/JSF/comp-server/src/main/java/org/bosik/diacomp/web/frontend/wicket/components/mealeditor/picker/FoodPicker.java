e124e4289766a6989fd279cb08f6b09b10094711
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2014-12-06 21:23:46 +0000

    Autocomplete food picker implemented.

---
+++ web/JSF/comp-server/src/main/java/org/bosik/diacomp/web/frontend/wicket/components/mealeditor/picker/FoodPicker.java
@@ -0,0 +1,70 @@
+package org.bosik.diacomp.web.frontend.wicket.components.mealeditor.picker;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior;
+import org.apache.wicket.extensions.ajax.markup.html.autocomplete.AutoCompleteTextField;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+import org.bosik.diacomp.core.entities.business.foodbase.FoodItem;
+import org.bosik.diacomp.core.entities.tech.Versioned;
+import org.bosik.diacomp.core.services.foodbase.FoodBaseService;
+import org.bosik.diacomp.web.backend.features.foodbase.service.FrontendFoodbaseService;
+
+public abstract class FoodPicker extends Panel
+{
+	private static final long	serialVersionUID	= 1L;
+
+	transient FoodBaseService	foodBase			= new FrontendFoodbaseService();
+
+	public FoodPicker(String id)
+	{
+		super(id);
+
+		Form<Void> form = new Form<Void>("form");
+		add(form);
+
+		final AutoCompleteTextField<String> field = new AutoCompleteTextField<String>("picker", new Model<String>(""))
+		{
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			protected Iterator<String> getChoices(String input)
+			{
+				List<String> choices = new ArrayList<String>();
+				List<Versioned<FoodItem>> list = foodBase.findAny(input);
+
+				for (final Versioned<FoodItem> item : list)
+				{
+					choices.add(item.getData().getName());
+				}
+
+				return choices.iterator();
+			}
+		};
+		form.add(field);
+
+		field.add(new AjaxFormSubmitBehavior(form, "onchange")
+		{
+			private static final long	serialVersionUID	= 1L;
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target)
+			{
+				String name = field.getModelObject();
+				Versioned<FoodItem> item = foodBase.findOne(name);
+				onSelected(item);
+			}
+
+			@Override
+			protected void onError(AjaxRequestTarget target)
+			{
+			}
+		});
+	}
+
+	public abstract void onSelected(Versioned<FoodItem> item);
+}
--- web/JSF/comp-server/src/main/java/org/bosik/diacomp/web/frontend/wicket/dialogs/disheditor/DishEditorContentPanel.java
+++ web/JSF/comp-server/src/main/java/org/bosik/diacomp/web/frontend/wicket/dialogs/disheditor/DishEditorContentPanel.java
@@ -4,8 +4,11 @@ import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.bosik.diacomp.core.entities.business.dishbase.DishItem;
+import org.bosik.diacomp.core.entities.business.foodbase.FoodItem;
 import org.bosik.diacomp.core.entities.tech.Versioned;
 import org.bosik.diacomp.core.utils.Utils;
+import org.bosik.diacomp.web.frontend.wicket.components.mealeditor.picker.FoodList;
+import org.bosik.diacomp.web.frontend.wicket.components.mealeditor.picker.FoodPicker;
 import org.bosik.diacomp.web.frontend.wicket.dialogs.common.CommonEditorContentPanel;
 
 public abstract class DishEditorContentPanel extends CommonEditorContentPanel<DishItem>
@@ -27,6 +30,33 @@ public abstract class DishEditorContentPanel extends CommonEditorContentPanel<Di
 		//FIXME
 
 		form.add(new TextField<String>("inputName", new PropertyModel<String>(entity, "data.name")));
+
+		FoodList list = new FoodList();
+		DishItem data = model.getObject().getData();
+		for (int i = 0; i < data.count(); i++)
+		{
+			list.getContent().add(data.get(i));
+		}
+
+		// Model.of(list)
+
+		form.add(new FoodPicker("editor")
+		{
+			private static final long	serialVersionUID	= 6850233237789079835L;
+
+			@Override
+			public void onSelected(Versioned<FoodItem> item)
+			{
+				if (item != null)
+				{
+					System.out.println("Selected: " + item.getData().getName());
+				}
+				else
+				{
+					System.out.println("Null selected");
+				}
+			}
+		});
 		//		form.add(new TextField<Double>("inputProts", new PropertyModel<Double>(dish, "data.relProts")));
 		//		form.add(new TextField<Double>("inputFats", new PropertyModel<Double>(dish, "data.relFats")));
 		//		form.add(new TextField<Double>("inputCarbs", new PropertyModel<Double>(dish, "data.relCarbs")));
