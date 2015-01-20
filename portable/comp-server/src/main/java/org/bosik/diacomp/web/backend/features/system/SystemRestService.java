d1730a69faab9ec7e9b842e91826d6f593788c33
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2015-01-20 01:22:47 +0000

    AuthRestService renamed to AuthRest.

--- portable/comp-server/src/main/java/org/bosik/diacomp/web/backend/features/system/SystemRestService.java
+++ portable/comp-server/src/main/java/org/bosik/diacomp/web/backend/features/system/SystemRestService.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Response.Status;
 import org.bosik.diacomp.core.rest.ResponseBuilder;
 import org.bosik.diacomp.core.rest.StdResponse;
 import org.bosik.diacomp.core.services.exceptions.CommonServiceException;
-import org.bosik.diacomp.web.backend.features.auth.rest.AuthRestService;
+import org.bosik.diacomp.web.backend.features.auth.rest.AuthRest;
 import org.json.JSONObject;
 
 @Path("info/")
@@ -28,8 +28,8 @@ public class SystemRestService
 			// Can't use StdResponse as far as response is JSON, not regular escaped string
 
 			JSONObject info = new JSONObject();
-			info.put("current", AuthRestService.API_CURRENT);
-			info.put("support", AuthRestService.API_LEGACY);
+			info.put("current", AuthRest.API_CURRENT);
+			info.put("support", AuthRest.API_LEGACY);
 
 			JSONObject resp = new JSONObject();
 			resp.put(StdResponse.TAG_CODE, ResponseBuilder.CODE_OK);
