847bb8a26126f6d7be950b86ad503de23af94b61
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2015-01-21 23:49:20 +0000

    ActivityDiary: event handling refactored. Common diary methods moved to PostprandUtils.

--- trunk/portable/comp-transfer/src/main/java/org/bosik/diacomp/core/services/diary/PostprandUtils.java
+++ trunk/portable/comp-transfer/src/main/java/org/bosik/diacomp/core/services/diary/PostprandUtils.java
@@ -1,5 +1,7 @@
 package org.bosik.diacomp.core.services.diary;
 
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import org.bosik.diacomp.core.entities.business.diary.DiaryRecord;
 import org.bosik.diacomp.core.entities.business.diary.records.BloodRecord;
@@ -58,12 +60,82 @@ public class PostprandUtils
 	}
 
 	/**
+	 * Deprecated. Use {@link #updatePostprand(List, int, int, int)} instead and pass the params directly.
 	 * 
 	 * @param records
 	 */
+	@Deprecated
 	public static void updatePostprand(List<Versioned<DiaryRecord>> records)
 	{
 		updatePostprand(records, DEFAULT_AFFECT_TIME_INSULIN, DEFAULT_AFFECT_TIME_MEAL_STD,
 				DEFAULT_AFFECT_TIME_MEAL_SHORT);
+	}
+
+	/**
+	 * Searches for the insulin injection nearest to the specified time
+	 * 
+	 * @param near
+	 *            Time to search around
+	 * @param scanPeriod
+	 *            , in seconds
+	 * @return
+	 */
+	public static InsRecord findNearestInsulin(DiaryService diary, Date near, long scanPeriod)
+	{
+		Date startTime = new Date(near.getTime() - (scanPeriod * Utils.MsecPerSec));
+		Date endTime = new Date(near.getTime() + (scanPeriod * Utils.MsecPerSec));
+		List<Versioned<DiaryRecord>> records = diary.findPeriod(startTime, endTime, false);
+
+		long min = scanPeriod * Utils.MsecPerSec * 2;
+		InsRecord nearestIns = null;
+
+		for (Versioned<DiaryRecord> record : records)
+		{
+			if (record.getData() instanceof InsRecord)
+			{
+				InsRecord currentIns = (InsRecord)record.getData();
+				long currentDist = Math.abs(currentIns.getTime().getTime() - near.getTime());
+				if (currentDist < min)
+				{
+					nearestIns = currentIns;
+					min = currentDist;
+				}
+			}
+		}
+
+		return nearestIns;
+	}
+
+	/**
+	 * Searches for the last BS record in period [since - scanPeriod, since]
+	 * 
+	 * @param since
+	 * @param scanPeriod
+	 *            , in seconds
+	 * 
+	 * @return
+	 */
+	public static BloodRecord findLastBlood(DiaryService diary, Date since, long scanPeriod, boolean skipPostprandials)
+	{
+		Date endTime = since;
+		Date startTime = new Date(endTime.getTime() - (scanPeriod * Utils.MsecPerSec));
+
+		List<Versioned<DiaryRecord>> records = diary.findPeriod(startTime, endTime, false);
+		updatePostprand(records);
+		Collections.reverse(records);
+
+		for (Versioned<DiaryRecord> record : records)
+		{
+			if (record.getData() instanceof BloodRecord)
+			{
+				BloodRecord bloodRecord = (BloodRecord)record.getData();
+				if (!skipPostprandials || !bloodRecord.isPostPrand())
+				{
+					return bloodRecord;
+				}
+			}
+		}
+
+		return null;
 	}
 }
