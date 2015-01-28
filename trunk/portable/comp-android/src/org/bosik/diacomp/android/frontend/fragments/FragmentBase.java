da66e15250295036b8edef4c3aa834b11fa7d79a
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2015-01-28 00:55:30 +0000

    Actions order.

--- trunk/portable/comp-android/src/org/bosik/diacomp/android/frontend/fragments/FragmentBase.java
+++ trunk/portable/comp-android/src/org/bosik/diacomp/android/frontend/fragments/FragmentBase.java
@@ -445,8 +445,8 @@ public class FragmentBase extends Fragment
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
 	{
-		super.onCreateOptionsMenu(menu, inflater);
 		inflater.inflate(R.menu.activity_base_actions, menu);
+		super.onCreateOptionsMenu(menu, inflater);
 	}
 
 	@Override
