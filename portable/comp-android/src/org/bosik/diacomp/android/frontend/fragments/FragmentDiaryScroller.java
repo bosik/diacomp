cfa3cabb4b17d34d2cdaef5614a4c6a604991f11
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2015-02-15 02:08:04 +0000

    Infinite scrolling refactored regarding Google guidelines.

--- portable/comp-android/src/org/bosik/diacomp/android/frontend/fragments/FragmentDiaryScroller.java
+++ portable/comp-android/src/org/bosik/diacomp/android/frontend/fragments/FragmentDiaryScroller.java
@@ -1,31 +1,21 @@
 package org.bosik.diacomp.android.frontend.fragments;
 
-import java.util.Calendar;
-import java.util.Date;
 import org.bosik.diacomp.android.R;
 import org.bosik.diacomp.android.frontend.views.diary.DiaryDayView;
-import org.bosik.diacomp.android.frontend.views.diary.DiaryDayView.OnLoadedListener;
-import org.bosik.diacomp.core.utils.Utils;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.AbsListView.OnScrollListener;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 
 public class FragmentDiaryScroller extends Fragment
 {
 	private static final String	TAG	= FragmentDiaryScroller.class.getSimpleName();
 
 	// Widgets
-	private ListView			list;
-	private int					n	= Integer.MAX_VALUE / 2;
+	private DiaryDayView		list;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -34,106 +24,9 @@ public class FragmentDiaryScroller extends Fragment
 
 		View rootView = inflater.inflate(R.layout.fragment_diary_scroller, container, false);
 
-		Calendar c = Calendar.getInstance();
-		c.set(Calendar.HOUR_OF_DAY, 0);
-		c.set(Calendar.MINUTE, 0);
-		c.set(Calendar.SECOND, 0);
-		c.set(Calendar.MILLISECOND, 0);
-		final Date baseDate = c.getTime();
-
 		// Widgets binding
-		list = (ListView) rootView.findViewById(R.id.listDiary);
-		final BaseAdapter adapter = new BaseAdapter()
-		{
-			@Override
-			public int getCount()
-			{
-				return Integer.MAX_VALUE;
-			}
-
-			@Override
-			public Object getItem(int pos)
-			{
-				return pos;
-			}
-
-			@Override
-			public long getItemId(int pos)
-			{
-				return pos;
-			}
-
-			@Override
-			public View getView(final int pos, View convertView, ViewGroup p)
-			{
-				final Date date = new Date(baseDate.getTime() + Utils.MsecPerDay * (pos - Integer.MAX_VALUE / 2));
-
-				// if (convertView == null)
-				// {
-				DiaryDayView diaryDayView = new DiaryDayView(FragmentDiaryScroller.this.getActivity(), date);
-
-				// }
-				// else
-				// {
-				// DiaryDayView diaryDayView = (DiaryDayView) convertView;
-				// diaryDayView.setDate(date);
-				// return diaryDayView;
-				// }
-
-				diaryDayView.setOnLoadedListener(new OnLoadedListener()
-				{
-					@Override
-					public void onLoaded(int contentHeight)
-					{
-						// FragmentDiaryScroller.this.list.scrollBy(0, -contentHeight);
-						Log.d(TAG, "Item at pos " + pos + " loaded with height " + contentHeight);
-						Log.d(TAG, "First visible: " + list.getFirstVisiblePosition());
-
-						if (pos <= list.getFirstVisiblePosition())
-						{
-							// list.scrollBy(0, contentHeight);
-						}
-					}
-				});
-				return diaryDayView;
-
-			}
-		};
-		list.setAdapter(adapter);
-		list.setSelection(Integer.MAX_VALUE / 2);
-		list.setOnScrollListener(new OnScrollListener()
-		{
-			@Override
-			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount)
-			{
-				// boolean loadMore = /* maybe add a padding */
-				// firstVisibleItem + visibleItemCount >= totalItemCount;
-				//
-				// if (loadMore)
-				// {
-				// count += visibleItemCount; // or any other amount
-				// adapter.notifyDataSetChanged();
-				// }
-				n = list.getFirstVisiblePosition();
-			}
-
-			@Override
-			public void onScrollStateChanged(AbsListView listView, int scrollState)
-			{
-				// if (scrollState == SCROLL_STATE_IDLE)
-				// {
-				// int threshold = 5;
-				// if (listView.getLastVisiblePosition() >= listView.getCount() - 1 - threshold)
-				// {
-				// currentPage++;
-				// load more list items:
-				// loadElements(currentPage);
-				// count += 30;
-				// adapter.notifyDataSetChanged();
-				// }
-				// }
-			}
-		});
+		list = (DiaryDayView) rootView.findViewById(R.id.listDiary);
+		// list.setDate(new Date());
 
 		Button button = (Button) rootView.findViewById(R.id.button1);
 		button.setOnClickListener(new OnClickListener()
@@ -141,8 +34,6 @@ public class FragmentDiaryScroller extends Fragment
 			@Override
 			public void onClick(View v)
 			{
-				n--;
-				list.setSelection(n);
 			}
 		});
 
--- portable/comp-android/src/org/bosik/diacomp/android/frontend/views/diary/DiaryDayView.java
+++ portable/comp-android/src/org/bosik/diacomp/android/frontend/views/diary/DiaryDayView.java
@@ -1,11 +1,11 @@
 package org.bosik.diacomp.android.frontend.views.diary;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import org.bosik.diacomp.android.R;
 import org.bosik.diacomp.android.backend.features.diary.DiaryLocalService;
-import org.bosik.diacomp.android.frontend.views.ExpandedListView;
 import org.bosik.diacomp.core.entities.business.diary.DiaryRecord;
 import org.bosik.diacomp.core.entities.business.diary.records.BloodRecord;
 import org.bosik.diacomp.core.entities.tech.Versioned;
@@ -13,50 +13,82 @@ import org.bosik.diacomp.core.services.diary.DiaryService;
 import org.bosik.diacomp.core.utils.Utils;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
 import android.widget.BaseAdapter;
+import android.widget.HeaderViewListAdapter;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class DiaryDayView extends LinearLayout
 {
-	public static interface OnLoadedListener
+	private static abstract class Item
 	{
-		void onLoaded(int contentHeight);
+		public abstract Date extractDate();
 	}
 
-	static final String						TAG		= DiaryDayView.class.getSimpleName();
+	private static class ItemHeader extends Item
+	{
+		public Date	date;
+
+		public ItemHeader(Date date)
+		{
+			this.date = date;
+		}
+
+		@Override
+		public Date extractDate()
+		{
+			return date;
+		}
+	}
+
+	private static class ItemData extends Item
+	{
+		public Versioned<? extends DiaryRecord>	record;
+
+		public ItemData(Versioned<? extends DiaryRecord> record)
+		{
+			this.record = record;
+		}
+
+		@Override
+		public Date extractDate()
+		{
+			return record.getData().getTime();
+		}
+	}
+
+	static final String	TAG				= DiaryDayView.class.getSimpleName();
 
 	// Data
-	private Date							date;
-	List<Versioned<? extends DiaryRecord>>	data	= new ArrayList<Versioned<? extends DiaryRecord>>();
-	OnLoadedListener						onLoadedListener;
+	private Date		date;
+	List<Item>			data			= new ArrayList<Item>();
+	boolean				loadingBefore	= false;
+	boolean				loadingAfter	= false;
 
 	// Components
-	private TextView						textDate;
-	ExpandedListView						listRecs;
-	ProgressBar								progressBar;
+	ListView			listRecs;
 
 	// Services
-	static DiaryService						diaryService;
+	static DiaryService	diaryService;
 
-	public DiaryDayView(Context context)
+	public DiaryDayView(final Context context)
 	{
-		// Should never be called actually
-		this(context, new Date());
+		this(context, null);
 	}
 
-	public DiaryDayView(Context context, Date date)
+	public DiaryDayView(final Context context, AttributeSet attributes)
 	{
 		super(context);
-		LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		final LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		inflater.inflate(R.layout.view_diary_day, this);
-
-		Log.d(TAG, "DiaryDayView for date " + date);
 
 		if (diaryService == null)
 		{
@@ -64,17 +96,50 @@ public class DiaryDayView extends LinearLayout
 			Log.i(TAG, "Diary service created");
 		}
 
-		textDate = (TextView) findViewById(R.id.textDayTitle);
-		listRecs = (ExpandedListView) findViewById(R.id.listRecs);
-		progressBar = (ProgressBar) findViewById(R.id.diaryProgressBar);
+		listRecs = (ListView) findViewById(R.id.listRecs);
 
-		this.date = date;
-		textDate.setText(Utils.formatDateLocal(date));
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.HOUR_OF_DAY, 0);
+		c.set(Calendar.MINUTE, 0);
+		c.set(Calendar.SECOND, 0);
+		c.set(Calendar.MILLISECOND, 0);
+
+		// FIXME: DEBUG ONLY
+		c.set(Calendar.DAY_OF_YEAR, 1);
+
+		setDate(c.getTime());
 
 		// ================================================================
 
+		View progressView = inflater.inflate(R.layout.view_diary_rec_loading, null);
+		listRecs.addHeaderView(progressView);
+		listRecs.addFooterView(progressView);
+
 		final BaseAdapter adapter = new BaseAdapter()
 		{
+			@Override
+			public int getViewTypeCount()
+			{
+				return 3;
+			}
+
+			@Override
+			public int getItemViewType(int position)
+			{
+				if (data.get(position) instanceof ItemHeader)
+				{
+					return 1;
+				}
+				else if (data.get(position) instanceof ItemData)
+				{
+					return 2;
+				}
+				else
+				{
+					return -17;
+				}
+			}
+
 			@Override
 			public int getCount()
 			{
@@ -95,103 +160,262 @@ public class DiaryDayView extends LinearLayout
 
 			@SuppressWarnings("unchecked")
 			@Override
-			public View getView(int pos, View v, ViewGroup p)
+			public View getView(int position, View convertView, ViewGroup parent)
 			{
-				View result;
-
-				Versioned<? extends DiaryRecord> record = data.get(pos);
-				DiaryRecord data = record.getData();
-				if (data instanceof BloodRecord)
+				synchronized (data)
 				{
-					DiaryRecBloodView rec = new DiaryRecBloodView(DiaryDayView.this.getContext());
-					rec.setData((Versioned<BloodRecord>) record);
-					result = rec;
+					int index = position;
+					Log.d(TAG,
+							String.format("getView() for position %d / FV: %d", position,
+									listRecs.getFirstVisiblePosition()));
+
+					Item item = data.get(index);
+
+					if (item instanceof ItemHeader)
+					{
+						if (convertView == null)
+						{
+							LayoutInflater inflater = (LayoutInflater) context
+									.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+							convertView = inflater.inflate(R.layout.view_diary_rec_header, null);
+						}
+
+						TextView textTitle = (TextView) convertView.findViewById(R.id.diaryDayHeader);
+						textTitle.setText(Utils.formatDateLocal(((ItemHeader) item).date));
+					}
+					else if (item instanceof ItemData)
+					{
+						Versioned<? extends DiaryRecord> record = ((ItemData) item).record;
+						DiaryRecord data = record.getData();
+						if (data instanceof BloodRecord)
+						{
+							DiaryRecBloodView rec = new DiaryRecBloodView(context);
+							rec.setData((Versioned<BloodRecord>) record);
+							convertView = rec;
+						}
+						else
+						{
+							throw new RuntimeException("Unsupported data type: " + data.getClass().getSimpleName());
+						}
+					}
+					else
+					{
+						throw new RuntimeException("Invalid data type: " + item);
+					}
+
+					return convertView;
 				}
-				else
-				{
-					// temp stub for unknown record types
-					DiaryRecBloodView rec = new DiaryRecBloodView(DiaryDayView.this.getContext());
-					rec.setData(new Versioned<BloodRecord>(new BloodRecord()));
-					result = rec;
-				}
-
-				// Log.v(TAG, "pos: " + pos + "; first: " +
-				// DiaryDayView.this.parentList.getFirstVisiblePosition());
-
-				// if (pos <= DiaryDayView.this.parentList.getFirstVisiblePosition())
-				// {
-				// result.measure(ViewGroup.LayoutParams.MATCH_PARENT,
-				// ViewGroup.LayoutParams.WRAP_CONTENT);
-				// int itemHeight = result.getMeasuredHeight();
-				// DiaryDayView.this.parentList.scrollBy(0, itemHeight);
-				// Log.v(TAG, "Scrolling to " + -itemHeight);
-				// }
-
-				return result;
 			}
 		};
 		listRecs.setAdapter(adapter);
-		updateContent();
+		listRecs.setSelection(1);
 
 		// ============================================================================
 
-		// listRecs.setOnFocusChangeListener(new OnFocusChangeListener()
-		// {
-		// @Override
-		// public void onFocusChange(View v, boolean hasFocus)
-		// {
-		// updateContent();
-		// }
-		// });
+		listRecs.setOnScrollListener(new OnScrollListener()
+		{
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState)
+			{
+			}
+
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount)
+			{
+				int threshold = 15;
+				if (firstVisibleItem < threshold)
+				{
+					loadBefore(getFirstDate());
+				}
+				else if (firstVisibleItem + visibleItemCount > totalItemCount - threshold)
+				{
+					loadAfter(getLastDate());
+				}
+			}
+		});
 	}
 
-	private void updateContent()
+	public void setDate(Date date)
 	{
+		synchronized (data)
+		{
+			// TODO: workaround
+			this.date = Utils.getNextDay(date);
+			data.clear();
+			loadAfter(date);
+		}
+	}
+
+	Date getFirstDate()
+	{
+		synchronized (data)
+		{
+			if (!data.isEmpty())
+			{
+				Item item = data.get(0);
+				return item.extractDate();
+			}
+			else
+			{
+				return date;
+			}
+		}
+	}
+
+	Date getLastDate()
+	{
+		synchronized (data)
+		{
+			Date result;
+			if (!data.isEmpty())
+			{
+				Item item = data.get(data.size() - 1);
+				result = item.extractDate();
+			}
+			else
+			{
+				result = date;
+			}
+
+			// to exclude the last date
+			return new Date(result.getTime() + 1000);
+		}
+	}
+
+	// TODO: optimize and move to Utils
+	static boolean sameDate(Date date1, Date date2)
+	{
+		return Utils.formatDateLocal(date1).equals(Utils.formatDateLocal(date2));
+	}
+
+	void loadBefore(Date date)
+	{
+		if (loadingBefore)
+		{
+			Log.w(TAG, "loadBefore() ignored");
+			return;
+		}
+
 		new AsyncTask<Date, Void, List<Versioned<? extends DiaryRecord>>>()
 		{
 			@Override
 			protected void onPreExecute()
 			{
-				progressBar.setVisibility(View.VISIBLE);
+				loadingBefore = true;
 			}
 
 			@Override
 			protected List<Versioned<? extends DiaryRecord>> doInBackground(Date... params)
 			{
-				return request(params[0]);
+				Date timeTo = params[0];
+				Date timeFrom = Utils.getPrevDay(timeTo);
+				Log.d(TAG, String.format("loadBefore(): %s and %s", timeFrom, timeTo));
+				return request(timeFrom, timeTo);
 			}
 
 			@Override
-			protected void onPostExecute(List<Versioned<? extends DiaryRecord>> result)
+			protected void onPostExecute(List<Versioned<? extends DiaryRecord>> records)
 			{
-				progressBar.setVisibility(View.GONE);
-				data = result;
-
-				((BaseAdapter) listRecs.getAdapter()).notifyDataSetChanged();
-
-				int contentHeight = updateHeight();
-				if (onLoadedListener != null)
+				synchronized (data)
 				{
-					onLoadedListener.onLoaded(contentHeight);
+					Date nextDate = getFirstDate();
+					int oldSize = data.size();
+
+					if (!records.isEmpty())
+					{
+						for (int i = records.size() - 1; i >= 0; i--)
+						{
+							Versioned<? extends DiaryRecord> record = records.get(i);
+							if (!sameDate(record.getData().getTime(), nextDate))
+							{
+								// TODO: it's time, not the date
+								data.add(0, new ItemHeader(nextDate));
+							}
+							data.add(0, new ItemData(record));
+							nextDate = record.getData().getTime();
+						}
+					}
+					else
+					{
+						data.add(0, new ItemHeader(Utils.getPrevDay(getFirstDate())));
+					}
+
+					final int delta = data.size() - oldSize;
+
+					Log.i(TAG, String.format("loadBefore(): %d new items loaded", delta));
+
+					final int oldFirst = listRecs.getFirstVisiblePosition();
+					((BaseAdapter) ((HeaderViewListAdapter) listRecs.getAdapter()).getWrappedAdapter())
+							.notifyDataSetChanged();
+					listRecs.setSelection(oldFirst + delta + 1);
+
+					loadingBefore = false;
 				}
 			}
 		}.execute(date);
 	}
 
-	int updateHeight()
+	void loadAfter(Date date)
 	{
-		// emulating height="wrap_content"
-		ViewGroup.LayoutParams params = listRecs.getLayoutParams();
-		params.height = listRecs.getFullHeight();
-		listRecs.setLayoutParams(params);
-		Log.v(TAG, "Full height: " + params.height);
-		return params.height;
+		if (loadingAfter)
+		{
+			Log.w(TAG, "loadAfter() ignored");
+			return;
+		}
+
+		new AsyncTask<Date, Void, List<Versioned<? extends DiaryRecord>>>()
+		{
+			@Override
+			protected void onPreExecute()
+			{
+				loadingAfter = true;
+			}
+
+			@Override
+			protected List<Versioned<? extends DiaryRecord>> doInBackground(Date... params)
+			{
+				Date timeFrom = params[0];
+				Date timeTo = Utils.getNextDay(timeFrom);
+				Log.d(TAG, String.format("loadAfter(): %s and %s", timeFrom, timeTo));
+				return request(timeFrom, timeTo);
+			}
+
+			@Override
+			protected void onPostExecute(List<Versioned<? extends DiaryRecord>> records)
+			{
+				synchronized (data)
+				{
+					Date prevDate = getLastDate();
+
+					if (!records.isEmpty())
+					{
+						for (int i = 0; i < records.size(); i++)
+						{
+							Versioned<? extends DiaryRecord> record = records.get(i);
+							if (!sameDate(prevDate, record.getData().getTime()))
+							{
+								// TODO: it's time, not the date
+								data.add(new ItemHeader(record.getData().getTime()));
+							}
+							data.add(new ItemData(record));
+							prevDate = record.getData().getTime();
+						}
+					}
+					else
+					{
+						data.add(new ItemHeader(Utils.getNextDay(getLastDate())));
+					}
+
+					loadingAfter = false;
+					((BaseAdapter) ((HeaderViewListAdapter) listRecs.getAdapter()).getWrappedAdapter())
+							.notifyDataSetChanged();
+				}
+			}
+		}.execute(date);
 	}
 
-	List<Versioned<? extends DiaryRecord>> request(Date date)
+	List<Versioned<? extends DiaryRecord>> request(Date startTime, Date endTime)
 	{
-		Date startTime = date;
-		Date endTime = Utils.getNextDay(date);
 		List<Versioned<DiaryRecord>> temp = diaryService.findPeriod(startTime, endTime, false);
 
 		List<Versioned<? extends DiaryRecord>> result = new ArrayList<Versioned<? extends DiaryRecord>>();
@@ -205,21 +429,5 @@ public class DiaryDayView extends LinearLayout
 		}
 
 		return result;
-	}
-
-	public Date getDate()
-	{
-		return date;
-	}
-
-	public void setDate(Date date)
-	{
-		this.date = date;
-		updateContent();
-	}
-
-	public void setOnLoadedListener(OnLoadedListener onLoadedListener)
-	{
-		this.onLoadedListener = onLoadedListener;
 	}
 }
