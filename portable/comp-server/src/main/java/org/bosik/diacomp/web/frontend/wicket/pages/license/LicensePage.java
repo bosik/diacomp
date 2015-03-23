6958c02f6fef1d99ef04d8a5636cb7c7d7f325db
bosikgn@gmail.com <bosikgn@gmail.com@1c9829cf-2f60-7538-583a-ff8dc639bea3>
2015-03-23 15:40:13 +0000

    License page implemented.

---
+++ portable/comp-server/src/main/java/org/bosik/diacomp/web/frontend/wicket/pages/license/LicensePage.java
@@ -0,0 +1,20 @@
+package org.bosik.diacomp.web.frontend.wicket.pages.license;
+
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.bosik.diacomp.web.frontend.wicket.pages.master.MasterPage;
+
+public class LicensePage extends MasterPage
+{
+	private static final long	serialVersionUID	= 1L;
+
+	public LicensePage(PageParameters parameters)
+	{
+		super(parameters);
+	}
+
+	@Override
+	protected void onInitialize()
+	{
+		super.onInitialize();
+	}
+}
