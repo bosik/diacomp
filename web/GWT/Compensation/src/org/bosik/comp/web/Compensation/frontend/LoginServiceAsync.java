60373e9e3bfa98575b769c39e75a8adb053d58c3
Nikita Bosik <bosiknk@rambler.ru>
2015-04-02 03:47:52 +0300

    /web folder removed.

--- web/GWT/Compensation/src/org/bosik/comp/web/Compensation/frontend/LoginServiceAsync.java
+++
@@ -1,8 +0,0 @@
-package org.bosik.comp.web.Compensation.frontend;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface LoginServiceAsync
-{
-	public void login(String requestUri, AsyncCallback<LoginInfo> async);
-}
