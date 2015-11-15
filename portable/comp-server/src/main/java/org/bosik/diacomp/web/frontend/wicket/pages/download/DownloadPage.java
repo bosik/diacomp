ceee6b619496c8275079e0c0c8c1a900a6553351
bosik <bosikgn@gmail.com>
2015-11-15 03:39:38 +0300

    Initialization Wicket components via constructors fixed.

--- portable/comp-server/src/main/java/org/bosik/diacomp/web/frontend/wicket/pages/download/DownloadPage.java
+++ portable/comp-server/src/main/java/org/bosik/diacomp/web/frontend/wicket/pages/download/DownloadPage.java
@@ -23,11 +23,17 @@ import org.bosik.diacomp.web.frontend.wicket.pages.master.MasterPage;
 
 public class DownloadPage extends MasterPage
 {
-	private static final long	serialVersionUID	= 1L;
+	private static final long serialVersionUID = 1L;
 
 	public DownloadPage(PageParameters parameters)
 	{
 		super(parameters);
+	}
+
+	@Override
+	protected void onInitialize()
+	{
+		super.onInitialize();
 		add(new ExternalLink("linkAppWindows", "api/windows/file/compensation_setup.exe"));
 	}
 }
