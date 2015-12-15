3af8b4560d3c507290a10ed68961895612c50160
bosik <bosikgn@gmail.com>
2015-12-15 03:36:52 +0300

    #59: Average BS chart introduced.

--- portable/comp-android/src/org/bosik/diacomp/android/frontend/fragments/FragmentCharts.java
+++ portable/comp-android/src/org/bosik/diacomp/android/frontend/fragments/FragmentCharts.java
@@ -18,17 +18,30 @@
  */
 package org.bosik.diacomp.android.frontend.fragments;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import org.bosik.diacomp.android.R;
 import org.bosik.diacomp.android.backend.features.analyze.KoofServiceInternal;
+import org.bosik.diacomp.android.backend.features.diary.LocalDiary;
 import org.bosik.diacomp.android.frontend.views.Chart;
 import org.bosik.diacomp.android.frontend.views.Chart.PostSetupListener;
 import org.bosik.diacomp.android.frontend.views.ProgressBundle.DataLoader;
+import org.bosik.diacomp.core.entities.business.diary.DiaryRecord;
+import org.bosik.diacomp.core.entities.business.diary.records.BloodRecord;
 import org.bosik.diacomp.core.services.analyze.KoofService;
 import org.bosik.diacomp.core.services.analyze.entities.Koof;
+import org.bosik.diacomp.core.services.diary.DiaryService;
 import org.bosik.diacomp.core.utils.Utils;
+import org.bosik.merklesync.Versioned;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LabelFormatter;
+import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.jjoe64.graphview.series.Series;
@@ -47,6 +60,8 @@ public class FragmentCharts extends Fragment
 	{
 		View rootView = inflater.inflate(R.layout.fragment_charts, container, false);
 
+		addChartBS();
+
 		addChartX();
 		addChartK();
 		addChartQ();
@@ -56,8 +71,131 @@ public class FragmentCharts extends Fragment
 
 	static double addRoom(double max)
 	{
-		double factor = 0.05 * 4;
-		return ((int) (1.1 * max / factor) + 1) * factor;
+		double top = 0.04;
+
+		while (top < max)
+		{
+			if (top * 2 > max)
+			{
+				return top * 2;
+			}
+			if (top * 5 > max)
+			{
+				return top * 5;
+			}
+			top *= 10;
+		}
+
+		return top;
+	}
+
+	private void addChartBS()
+	{
+		Chart chart = (Chart) getChildFragmentManager().findFragmentById(R.id.chartBS);
+		if (chart == null)
+		{
+			chart = new Chart();
+			getChildFragmentManager().beginTransaction().add(R.id.chartBS, chart).commit();
+		}
+
+		chart.setTitle(getString(R.string.charts_average_bs));
+		chart.setDataLoader(new DataLoader()
+		{
+			@Override
+			public Series<?> load(ContentResolver contentResolver)
+			{
+				final int WINDOW_SIZE = 7; // days
+				final int PERIOD = 30; // days
+
+				// PREPARE DATE TREE
+
+				DiaryService diary = LocalDiary.getInstance(contentResolver);
+				Date endTime = new Date();
+				Date startTime = Utils.shiftDate(endTime, -PERIOD - WINDOW_SIZE);
+				List<Versioned<DiaryRecord>> recs = diary.findPeriod(startTime, endTime, false);
+
+				SortedMap<Date, Double> bs = new TreeMap<>();
+				for (Versioned<DiaryRecord> rec : recs)
+				{
+					if (rec.getData() instanceof BloodRecord)
+					{
+						BloodRecord blood = (BloodRecord) rec.getData();
+						bs.put(blood.getTime(), blood.getValue());
+					}
+				}
+
+				// ANALYZE & FILL SERIES
+
+				List<DataPoint> dataList = new ArrayList<DataPoint>();
+
+				for (int i = 0; i < PERIOD; i++)
+				{
+					Date windowStart = Utils.shiftDate(startTime, i);
+					Date windowEnd = Utils.shiftDate(startTime, i + WINDOW_SIZE);
+					SortedMap<Date, Double> items = bs.subMap(windowStart, windowEnd);
+
+					if (!items.isEmpty())
+					{
+						Collection<Double> values = items.values();
+
+						double mean = Utils.getMean(values);
+						// double deviation = Utils.getDeviation(values, mean);
+						dataList.add(new DataPoint(windowEnd, mean));
+					}
+					else
+					{
+						dataList.add(new DataPoint(windowEnd, 0));
+					}
+				}
+
+				DataPoint[] data = dataList.toArray(new DataPoint[dataList.size()]);
+				LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(data);
+				series.setColor(Color.rgb(255, 0, 0));
+
+				return series;
+			}
+		});
+		chart.setPostSetupListener(new PostSetupListener()
+		{
+			@Override
+			public void onPostSetup(Chart chart)
+			{
+				final GraphView graphView = chart.getGraphView();
+
+				graphView.getViewport().setXAxisBoundsManual(true);
+				graphView.getViewport().setYAxisBoundsManual(true);
+				graphView.getViewport().setMinY(0);
+
+				if (!graphView.getSeries().isEmpty())
+				{
+					Series<?> series = graphView.getSeries().get(0);
+					graphView.getViewport().setMaxY(addRoom(series.getHighestValueY()));
+					graphView.getViewport().setMinX(series.getLowestValueX());
+					graphView.getViewport().setMaxX(series.getHighestValueX());
+				}
+
+				graphView.getGridLabelRenderer().setLabelFormatter(new LabelFormatter()
+				{
+					@Override
+					public String formatLabel(double value, boolean isValueX)
+					{
+						if (isValueX)
+						{
+							return new SimpleDateFormat("dd/MM", Locale.US).format(new Date((long) value));
+						}
+						else
+						{
+							return String.format(Locale.US, "%.1f", value);
+						}
+					}
+
+					@Override
+					public void setViewport(Viewport arg0)
+					{
+					}
+				});
+			}
+		});
 	}
 
 	private void addChartX()
