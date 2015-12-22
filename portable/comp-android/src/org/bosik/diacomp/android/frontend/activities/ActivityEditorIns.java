054889235485d10e020cf00bbe508de54f8e90f1
bosik <bosikgn@gmail.com>
2015-12-22 05:35:42 +0300

    ActivityEditorBlood & ActivityEditorIns: input field improved.

--- portable/comp-android/src/org/bosik/diacomp/android/frontend/activities/ActivityEditorIns.java
+++ portable/comp-android/src/org/bosik/diacomp/android/frontend/activities/ActivityEditorIns.java
@@ -22,6 +22,7 @@ import java.util.Date;
 import org.bosik.diacomp.android.R;
 import org.bosik.diacomp.android.frontend.UIUtils;
 import org.bosik.diacomp.core.entities.business.diary.records.InsRecord;
+import org.bosik.diacomp.core.utils.Utils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -117,7 +118,7 @@ public class ActivityEditorIns extends ActivityEditorTime<InsRecord>
 
 		try
 		{
-			entity.getData().setValue(Double.parseDouble(editValue.getText().toString()));
+			entity.getData().setValue(Utils.parseExpression(editValue.getText().toString()));
 		}
 		catch (NumberFormatException e)
 		{
