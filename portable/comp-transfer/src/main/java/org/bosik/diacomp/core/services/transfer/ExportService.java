29c256be53d1c213fdff4556d8f1a8c991a6c90a
bosik <bosikgn@gmail.com>
2017-04-30 13:21:04 +0300

    #91: ExportService & ImportService introduced.

---
+++ portable/comp-transfer/src/main/java/org/bosik/diacomp/core/services/transfer/ExportService.java
@@ -0,0 +1,6 @@
+package org.bosik.diacomp.core.services.transfer;
+
+public interface ExportService
+{
+	String exportData();
+}
---
+++ portable/comp-transfer/src/main/java/org/bosik/diacomp/core/services/transfer/ImportService.java
@@ -0,0 +1,6 @@
+package org.bosik.diacomp.core.services.transfer;
+
+public interface ImportService
+{
+	void importData(String data);
+}
