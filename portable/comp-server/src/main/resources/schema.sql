286520216b5534f066d6b8273f3f107c5307e2fa
Nikita Bosik <bosikgn@gmail.com>
2017-07-12 18:16:45 +0300

    SQL schema updated

--- portable/comp-server/src/main/resources/schema.sql
+++ portable/comp-server/src/main/resources/schema.sql
@@ -1,27 +1,24 @@
 delimiter $$
 
-CREATE DATABASE `compensation` /*!40100 DEFAULT CHARACTER SET cp1251 */$$
-
-delimiter $$
+CREATE DATABASE `compensation` /*!40100 DEFAULT CHARACTER SET utf8 */$$
 
 CREATE TABLE `diary2` (
-  `_GUID` char(32) CHARACTER SET armscii8 NOT NULL,
+  `_GUID` char(32) NOT NULL,
   `_UserID` int(10) unsigned NOT NULL,
-  `_TimeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `_Hash` char(32) NOT NULL,
+  `_TimeStamp` datetime NOT NULL,
   `_Version` int(11) NOT NULL,
   `_Deleted` int(1) DEFAULT '0',
   `_Content` text,
-  `_TimeCache` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `_TimeCache` datetime NOT NULL,
   PRIMARY KEY (`_GUID`)
-) ENGINE=MyISAM AUTO_INCREMENT=1526 DEFAULT CHARSET=utf8$$
-
-
-delimiter $$
+) ENGINE=InnoDB AUTO_INCREMENT=1526 DEFAULT CHARSET=utf8$$
 
 CREATE TABLE `dishbase2` (
   `_GUID` char(32) NOT NULL,
   `_UserID` int(11) NOT NULL,
-  `_TimeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `_Hash` char(32) NOT NULL,
+  `_TimeStamp` datetime NOT NULL,
   `_Version` int(11) NOT NULL,
   `_Deleted` int(1) NOT NULL DEFAULT '0',
   `_Content` text NOT NULL,
@@ -29,14 +26,12 @@ CREATE TABLE `dishbase2` (
   PRIMARY KEY (`_GUID`),
   UNIQUE KEY `_GUID_UNIQUE` (`_GUID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
-
-
-delimiter $$
 
 CREATE TABLE `foodbase2` (
   `_GUID` char(32) NOT NULL,
   `_UserID` int(11) NOT NULL,
-  `_TimeStamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `_Hash` char(32) NOT NULL,
+  `_TimeStamp` datetime NOT NULL,
   `_Version` int(11) NOT NULL,
   `_Deleted` int(1) NOT NULL DEFAULT '0',
   `_Content` text NOT NULL,
@@ -45,30 +40,26 @@ CREATE TABLE `foodbase2` (
   UNIQUE KEY `_GUID_UNIQUE` (`_GUID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
 
-
-delimiter $$
-
 CREATE TABLE `user` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Login` varchar(40) CHARACTER SET cp1251 NOT NULL,
-  `HashPass` char(32) CHARACTER SET cp1251 COLLATE cp1251_general_cs NOT NULL,
-  `DateReg` datetime DEFAULT NULL,
+  `Login` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `HashPass` char(70) NOT NULL,
+  `ActivationKey` char(64) DEFAULT NULL,
+  `DateReg` datetime NOT NULL,
   `DateLogin` datetime DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci$$
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci$$
 
-
-INSERT INTO `user`
-(`ID`,
-`Login`,
-`HashPass`,
-`DateReg`,
-`DateLogin`)
-VALUES
-(
-1,
-"bosik-007@narod.ru",
-"4761fe12f59bb38d3faca218bcffdaa3",
-"2012-01-05 14:41:01",
-"2014-02-25 20:10:34"
+INSERT INTO `user` (
+  `ID`,
+  `Login`,
+  `HashPass`,
+  `DateReg`,
+  `DateLogin`)
+VALUES (
+  1,
+  "developer",
+  "1024:13a65e564d9d854245331816d8d8eaac:069859f6562795f6b8527dec06bf2e64",
+  "2012-01-05 14:41:01",
+  "2014-02-25 20:10:34"
 );
