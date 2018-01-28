ff5bc3d356245408e2df32ebcc0a6ec543860fd9
bosik <bosikgn@gmail.com>
2018-01-28 21:21:12 +0300

    CollectionUtils introduced

---
+++ portable/comp-transfer/src/main/java/org/bosik/diacomp/core/utils/CollectionUtils.java
@@ -0,0 +1,44 @@
+/*
+ * Diacomp - Diabetes analysis & management system
+ * Copyright (C) 2013 Nikita Bosik
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.bosik.diacomp.core.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionUtils
+{
+	public interface Predicate<T>
+	{
+		boolean test(T e);
+	}
+
+	public static <T> List<T> filter(Iterable<T> items, Predicate<T> predicate)
+	{
+		List<T> result = new ArrayList<>();
+
+		for (T item : items)
+		{
+			if (predicate.test(item))
+			{
+				result.add(item);
+			}
+		}
+
+		return result;
+	}
+}
