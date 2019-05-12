61ec91e5e59c55f91420b72b10a49f85fb3cfc87
bosik <bosikgn@gmail.com>
2019-05-13 00:18:35 +0300

    #182: Configured to run in Tomcat; dependencies cleanup

--- portable/comp-server-rest/src/main/java/org/bosik/diacomp/web/backend/DiacompRestApplication.java
+++ portable/comp-server-rest/src/main/java/org/bosik/diacomp/web/backend/DiacompRestApplication.java
@@ -2,16 +2,24 @@ package org.bosik.diacomp.web.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class DiacompRestApplication
+public class DiacompRestApplication extends SpringBootServletInitializer
 {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(DiacompRestApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(DiacompRestApplication.class);
 	}
 
 	@Configuration
